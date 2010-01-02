804216563afacc69d8b93449cf3e6d7e9bbb474d
Fahad Ibnay Heylaal <fahad19@gmail.com>
2010-01-02 15:14:48 +0600

    commenting primeminister's timezone fix for strict warnings in PHP 5.3

--- app/config/core.php
+++ app/config/core.php
@@ -175,7 +175,7 @@
  * If you are on PHP 5.3 uncomment this line and correct your server timezone
  * to fix the date & time related errors.
  */
-	date_default_timezone_set('UTC');
+	//date_default_timezone_set('UTC');
 
 /**
  *
