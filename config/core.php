1db09dbefaa46d34a09726942fd666552bf5516a
Fahad Ibnay Heylaal <fahad19@gmail.com>
2010-03-31 02:09:21 +0600

    modifying salt and seed value to keep it consistent with croogo_data.sql. they will be changed by Install plugin anyway.

--- config/core.php
+++ config/core.php
@@ -204,12 +204,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi19');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '7685930965745354249674968364519');
 
 /**
  * Compress CSS output by removing comments, whitespace, repeating tags, etc.
