40ce30b7587c1bc520f841ebdf3fb91166f2b07d
Fahad Ibnay Heylaal <fahad19@gmail.com>
2010-08-24 00:44:30 +0700

    formatting and fix for conflict.

--- plugins/tinymce/config/tinymce_bootstrap.php
+++ plugins/tinymce/config/tinymce_bootstrap.php
@@ -2,37 +2,23 @@
 /**
  * Configuration
  */
-<<<<<<< HEAD
     Configure::write('Tinymce.actions', array(
         'Nodes/admin_add' => array(
-            'elements' => 'NodeBody',
+            array(
+                'elements' => 'NodeBody',
+            ),
         ),
         'Nodes/admin_edit' => array(
-            'elements' => 'NodeBody',
+            array(
+                'elements' => 'NodeBody',
+            ),
         ),
         'Translate/admin_edit' => array(
-            'elements' => 'NodeBody',
+            array(
+                'elements' => 'NodeBody',
+            ),
         ),
     ));
-=======
-Configure::write('Tinymce.actions', array(
-    'Nodes/admin_add' => array(
-        array(
-            'elements' => 'NodeBody',
-        ),
-    ),
-    'Nodes/admin_edit' => array(
-        array(
-            'elements' => 'NodeBody',
-        ),
-    ),
-    'Translate/admin_edit' => array(
-        array(
-            'elements' => 'NodeBody',
-        ),
-    ),
-));
->>>>>>> 937b76aadcbf59da40a8b613bba6483e8be2f413
 
 /**
  * Hook helper
