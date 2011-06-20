7224a009881e9e53f0a94132af10766b95897048
Rachman Chavik <rchavik@xintesa.com>
2011-06-20 10:03:21 +0700

    acl overhaul: componentize  __generateAcoTreelist

--- Plugin/Acl/Controller/Component/AclFilterComponent.php
+++ Plugin/Acl/Controller/Component/AclFilterComponent.php
@@ -149,5 +149,128 @@ class AclFilterComponent extends Component {
         return array('authorized' => $authorized, 'allowed' => $allowedActions);
     }
 
+/** __formatAcoTree
+ *
+ *  Internally used by AclFilter::acoTreelist.
+ *  An extra field is created for the number of children for each ACO.
+ *
+ *  @param tree tree/subtree of ACOs retrieved via find('threaded')
+ *  @return array TreeBehavior::generatetreelist compatible array
+ *  @see AclFilterComponent::acoTreelist
+ */
+    private function __formatAcoTree($tree, $parent = false, $level = 0) {
+        static $acos = array();
+        foreach ($tree as $i => $leaf) {
+            $alias = str_pad($leaf['Aco']['alias'], strlen($leaf['Aco']['alias']) + $level, '-', STR_PAD_LEFT);
+            if ($parent) {
+                $path = $parent . '/' . $leaf['Aco']['alias'];
+            } else {
+                $path = $leaf['Aco']['alias'];
+            }
+            $acos[$leaf['Aco']['id']] = array(
+                $alias,
+                'path' => $path,
+                );
+            if (!empty($leaf['children'])) {
+                $this->__formatAcoTree($leaf['children'], $path, ++$level);
+                $level--;
+                $children = count($leaf['children']);
+                $acos[$leaf['Aco']['id']]['children'] = $children;
+            } else {
+                $acos[$leaf['Aco']['id']]['children'] = 0;
+            }
+        }
+        return $acos;
+    }
+
+/** acoTreeList
+ *
+ *  Recursive function to construct a generatetreelist() compatible array.
+ */
+    public function acoTreelist() {
+        $acoConditions = array(
+            'parent_id !=' => null,
+            //'model' => null,
+            'foreign_key' => null,
+            'alias !=' => null,
+        );
+        $acos = $this->controller->Acl->Aco->find('threaded', array('conditions' => $acoConditions));
+        $acos = $this->__formatAcoTree($acos);
+
+        // calculate the number of grandchildren for each ACO
+        $paths = Set::extract('/path', $acos);
+        foreach ($acos as $id => &$aco) {
+            if (strpos($aco['path'], '/') === false) {
+                $childcount = count(preg_grep('/^'. $aco['path'] . '\//', $paths));
+            } else {
+                $aco['grandchildren'] = 0;
+                continue;
+            }
+            if ($aco['children'] == 0 || $aco['children'] == $childcount) {
+                $aco['grandchildren'] = 0;
+            } else {
+                $aco['grandchildren'] = $childcount;
+            }
+        }
+
+        // now that we have count of children and grandchildren, populate the rest of fields
+        foreach ($acos  as $id => &$aco) {
+            $path = $aco['path'];
+            $childcount = $aco['children'];
+
+            // determine type of ACO
+            if ($childcount == 0) {
+                $type = 'action';
+            } else {
+                if ($aco['grandchildren'] > 0) {
+                    $type = 'plugin';
+                    if ($aco['grandchildren'] == $aco['children']) {
+                        $type = 'controller';
+                    }
+                } else {
+                    $type = 'controller';
+                }
+            }
+
+            // determine value of plugin, controller, and action names
+            if ($type == 'plugin') {
+                $plugin = $path; $controller = false; $action = false;
+            } else {
+                $c = substr_count($path, '/');
+                $pathE = explode('/', $path);
+                if ($type == 'action') {
+                    if ($c == 2) {
+                        $plugin = $pathE[0];
+                        $controller = $pathE[1];
+                        $action = $pathE[2];
+                    } elseif ($c == 1) {
+                        $plugin = false;
+                        $controller = $pathE[0];
+                        $action = $pathE[1];
+                    }
+                } elseif ($type == 'controller') {
+                    if ($c == 1) {
+                        $plugin = $pathE[0];
+                        $controller = $pathE[1];
+                        $action = '';
+                    } elseif ($c == 0) {
+                        $plugin = false;
+                        $controller = $pathE[0];
+                        $action = '';
+                    }
+                }
+            }
+
+            $aco = Set::merge($aco, array(
+                'type' => $type,
+                'children' => $childcount,
+                'plugin' => $plugin,
+                'controller' => $controller,
+                'action' => $action,
+                ));
+        }
+        return $acos;
+    }
+
 }
 ?>
