c441c4f57ba29b66c0dd557e8b4890a1933a1bc5
Rachman Chavik <rchavik@xintesa.com>
2011-07-08 13:03:49 +0700

    don't hijack admin settings

--- plugins/acl/controllers/components/acl_filter.php
+++ plugins/acl/controllers/components/acl_filter.php
@@ -35,7 +35,9 @@ class AclFilterComponent extends Object {
             'controller' => 'users',
             'action' => 'login',
         );
-        $loginAction = Set::merge($loginAction, Configure::read('Acl.Auth.loginAction'));
+        if (!isset($this->controller->params['admin'])) {
+            $loginAction = Set::merge($loginAction, Configure::read('Acl.Auth.loginAction'));
+        }
         $this->controller->Auth->loginAction = $loginAction;
 
         $logoutRedirect = array(
