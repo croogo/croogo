73b2263dfc3dbd096d4a1f4456c21281e8cafddf
Fahad Ibnay Heylaal <fahad19@gmail.com>
2011-08-20 03:37:33 +0100

    fix for ACO generation.

--- plugins/acl/controllers/components/acl_generate.php
+++ plugins/acl/controllers/components/acl_generate.php
@@ -71,7 +71,7 @@ class AclGenerateComponent extends Object {
  */
     public function listActions($name, $path) {
         // base methods
-        if (strpos($path, APP.'plugins') !== false) {
+        if (strstr($path, APP .'plugins')) {
             $plugin = $this->getPluginFromPath($path);
             $pacName = Inflector::camelize($plugin) . 'AppController'; // pac - PluginAppController
             $pacPath = APP.'plugins'.DS.$plugin.DS.$plugin.'_app_controller.php';
