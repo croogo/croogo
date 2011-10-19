39539de9355e3e76439eb43974125a0b7c49435c
Rachman Chavik <rchavik@xintesa.com>
2011-10-19 09:48:03 +0700

    updating model test groups

---
+++ Test/Case/AllModelsTest.php
@@ -0,0 +1,11 @@
+<?php
+class AllModelsTest extends PHPUnit_Framework_TestSuite {
+
+    public static function suite() {
+		$suite = new CakeTestSuite('All model tests');
+		$path = APP . 'Test' .DS. 'Case' .DS. 'Model' .DS;
+        $suite->addTestDirectory($path);
+		return $suite;
+    }
+}
+?>
