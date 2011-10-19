009543949e864748a92ebdb8af58378ee204ac17
Rachman Chavik <rchavik@xintesa.com>
2011-10-19 09:43:54 +0700

    updating behavior test groups

---
+++ Test/Case/AllBehaviorsTest.php
@@ -0,0 +1,11 @@
+<?php
+class AllBehaviorsTest extends PHPUnit_Framework_TestSuite {
+
+    public static function suite() {
+		$suite = new CakeTestSuite('All behavior tests');
+		$path = APP . 'Test' .DS. 'Case' .DS. 'Model' .DS. 'Behavior' .DS;
+        $suite->addTestDirectory($path);
+		return $suite;
+    }
+}
+?>
