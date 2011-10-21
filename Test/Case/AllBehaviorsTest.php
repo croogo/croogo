c703e3e56101ab47ba5b1ef2216dc0f89ba29363
Rachman Chavik <rchavik@xintesa.com>
2011-10-21 10:11:51 +0700

    updating and adding group tests

--- Test/Case/AllBehaviorsTest.php
+++ Test/Case/AllBehaviorsTest.php
@@ -2,10 +2,10 @@
 class AllBehaviorsTest extends PHPUnit_Framework_TestSuite {
 
     public static function suite() {
-		$suite = new CakeTestSuite('All behavior tests');
-		$path = APP . 'Test' .DS. 'Case' .DS. 'Model' .DS. 'Behavior' .DS;
+        $suite = new CakeTestSuite('All behavior tests');
+        $path = APP . 'Test' .DS. 'Case' .DS. 'Model' .DS. 'Behavior' .DS;
         $suite->addTestDirectory($path);
-		return $suite;
+        return $suite;
     }
 }
 ?>
---
+++ Test/Case/AllHelpersTest.php
@@ -0,0 +1,11 @@
+<?php
+class AllHelpersTest extends PHPUnit_Framework_TestSuite {
+
+    public static function suite() {
+        $suite = new CakeTestSuite('All helper tests');
+        $path = APP . 'Test' .DS. 'Case' .DS. 'View' .DS. 'Helper' .DS;
+        $suite->addTestDirectory($path);
+        return $suite;
+    }
+}
+?>
