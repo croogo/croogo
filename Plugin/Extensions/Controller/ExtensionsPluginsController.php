954b4f900144b2cccf59900d68a0e15170d66052
Rachman Chavik <rchavik@xintesa.com>
2011-11-30 09:50:56 +0700

    changing path checks from 'plugins' to 'Plugin'

--- Plugin/Extensions/Controller/ExtensionsPluginsController.php
+++ Plugin/Extensions/Controller/ExtensionsPluginsController.php
@@ -62,9 +62,9 @@ class ExtensionsPluginsController extends AppController {
     public function admin_add() {
         $this->set('title_for_layout', __('Upload a new plugin'));
 
-        if (!empty($this->data)) {
-            $file = $this->data['Plugin']['file'];
-            unset($this->data['Plugin']['file']);
+        if (!empty($this->request->data)) {
+            $file = $this->request->data['Plugin']['file'];
+            unset($this->request->data['Plugin']['file']);
 
             // get plugin name and root
             $zip = zip_open($file['tmp_name']);
@@ -101,7 +101,7 @@ class ExtensionsPluginsController extends AppController {
 
             $pluginName = $plugin;
 
-            if (is_dir(APP . 'plugins' . DS . $pluginName)) {
+            if (is_dir(APP . 'Plugin' . DS . $pluginName)) {
                 $this->Session->setFlash(__('Plugin already exists.'), 'default', array('class' => 'error'));
                 $this->redirect(array('action' => 'add'));
             }
@@ -110,15 +110,15 @@ class ExtensionsPluginsController extends AppController {
             $zip = zip_open($file['tmp_name']);
             if ($zip) {
                 // create root plugin dir
-                $path = APP . 'plugins' . DS . $pluginName . DS;
+                $path = APP . 'Plugin' . DS . $pluginName . DS;
                 mkdir($path);
                 while ($zip_entry = zip_read($zip)) {
                     $zipEntryName = zip_entry_name($zip_entry);
                     $zipEntryNameE = array_slice(explode('/', $zipEntryName), $root);
                     if (!empty($zipEntryNameE[count($zipEntryNameE)-1])) {
-                        $path = APP . 'plugins' . DS . $pluginName . DS . implode(DS, $zipEntryNameE);
+                        $path = APP . 'Plugin' . DS . $pluginName . DS . implode(DS, $zipEntryNameE);
                     } else {
-                        $path = APP . 'plugins' . DS . $pluginName . DS . implode(DS, $zipEntryNameE) . DS;
+                        $path = APP . 'Plugin' . DS . $pluginName . DS . implode(DS, $zipEntryNameE) . DS;
                     }
                     if (substr($path, strlen($path) - 1) == DS) {
                         // create directory
@@ -155,7 +155,7 @@ class ExtensionsPluginsController extends AppController {
         }
 
         $folder =& new Folder;
-        if ($folder->delete(APP . 'plugins' . DS . $plugin)) {
+        if ($folder->delete(APP . 'Plugin' . DS . $plugin)) {
             $this->Session->setFlash(__('Plugin deleted successfully.'), 'default', array('class' => 'success'));
         } else {
             $this->Session->setFlash(__('Plugin could not be deleted.'), 'default', array('class' => 'error'));
