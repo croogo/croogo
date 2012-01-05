425180aaf3d2aefcd0fbf1facdd5b965f9d50a43
Fahad Ibnay Heylaal <fahad19@gmail.com>
2012-01-05 00:11:08 +0000

    test case to prove that CroogoNav::add() is not working when menus are two levels deep.
    for example: CroogoNav::add('extensions.children.plugins.children.example', ...);

---
+++ Test/Case/Lib/CroogoNavTest.php
@@ -0,0 +1,38 @@
+<?php
+App::import('Lib', 'CroogoNav');
+
+class CroogoNavTest extends CakeTestCase {
+    
+    public function testNav() {
+        
+        // test clear
+        CroogoNav::clear();
+        $items = CroogoNav::items();
+        $this->assertEqual($items, array());
+        
+        // test first level addition
+        $defaults = CroogoNav::getDefaults();
+        $extensions = array('title' => 'Extensions');
+        CroogoNav::add('extensions', $extensions);
+        $result = CroogoNav::items();
+        $expected = array('extensions' => Set::merge($defaults, $extensions));
+        $this->assertEqual($result, $expected);
+        
+        // tested nested insertion (1 level)
+        $plugins = array('title' => 'Plugins');
+        CroogoNav::add('extensions.children.plugins', $plugins);
+        $result = CroogoNav::items();
+        $expected['extensions']['children']['plugins'] = Set::merge($defaults, $plugins);
+        $this->assertEqual($result, $expected);
+        
+        // 2 levels deep
+        $example = array('title' => 'Example');
+        CroogoNav::add('extensions.children.plugins.children.example', $plugins);
+        $result = CroogoNav::items();
+        
+        $expected['extensions']['children']['plugins']['children']['example'] = Set::merge($defaults, $example);
+        $this->assertEqual($result, $expected);
+    }
+    
+}
+?>
