005c33196239702006450d502ddda78335fabaad
Rachman Chavik <rchavik@xintesa.com>
2012-01-12 17:01:23 +0700

    pluggable-auth: subclass default AclBehavior
    CroogoAclBehavior does not raise warnings

---
+++ Model/Behavior/CroogoAclBehavior.php
@@ -0,0 +1,25 @@
+<?php
+
+App::uses('AclBehavior', 'Model/Behavior');
+
+class CroogoAclBehavior extends AclBehavior {
+
+    public function setup($model, $config = array()) {
+        if (isset($config[0])) {
+            $config['type'] = $config[0];
+            unset($config[0]);
+        }
+        $this->settings[$model->name] = array_merge(array('type' => 'controlled'), $config);
+        $this->settings[$model->name]['type'] = strtolower($this->settings[$model->name]['type']);
+
+        $types = $this->_typeMaps[$this->settings[$model->name]['type']];
+
+        if (!is_array($types)) {
+            $types = array($types);
+        }
+        foreach ($types as $type) {
+            $model->{$type} = ClassRegistry::init($type);
+        }
+    }
+
+}
