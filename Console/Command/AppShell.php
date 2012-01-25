262b771d1036f62636409f4698b3bf16ed9a81eb
Rachman Chavik <rchavik@xintesa.com>
2012-01-25 13:11:35 +0700

    adding AppShell for cakephp 2.1

---
+++ Console/Command/AppShell.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Croogo AppShell
+ *
+ * PHP version 5
+ *
+ * @category Shell
+ * @package  Croogo
+ * @version  1.0
+ * @author   Fahad Ibnay Heylaal <contact@fahad19.com>
+ * @license  http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @link     http://www.croogo.org
+ */
+
+App::uses('Shell', 'Console');
+
+class AppShell extends Shell {
+
+}
