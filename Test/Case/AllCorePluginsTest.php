0f1178cafc011f997b13a924e16f09642df611a7
Kyle Robinson Young <kyle@dontkry.com>
2012-06-15 12:05:38 -0700

    Include Extensions, FileManager and Tinymce in AllCoreTests

--- Test/Case/AllCorePluginsTest.php
+++ Test/Case/AllCorePluginsTest.php
@@ -9,10 +9,13 @@ class AllCorePluginsTest extends PHPUnit_Framework_TestSuite {
 
 	public static function suite() {
 		$suite = new CakeTestSuite('All core plugins tests');
-		$path = CakePlugin::path('Acl') . 'Test' . DS . 'Case' . DS;
-		$suite->addTestFile($path . 'AllAclTestsTest.php');
-		$path = CakePlugin::path('Extensions') . DS . 'Test' . DS . 'Case' . DS;
-		$suite->addTestFile($path . 'AllExtensionsTestsTest.php');
+		$plugins = array(
+			'Acl', 'Extensions', 'FileManager', 'Tinymce',
+		);
+		foreach ($plugins as $plugin) {
+			CakePlugin::load($plugin);
+			$suite->addTestDirectoryRecursive(CakePlugin::path($plugin) . 'Test' . DS);
+		}
 		return $suite;
 	}
 
