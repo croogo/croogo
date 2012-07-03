dc4a41b56f74d7b1ca24d6b0a97ef9d17ddb3434
Kyle Robinson Young <kyle@dontkry.com>
2012-07-03 13:42:11 -0700

    Install with composer wrapper

---
+++ Plugin/Extensions/Lib/CroogoComposer.php
@@ -0,0 +1,132 @@
+<?php
+App::uses('File', 'Utility');
+App::uses('HttpSocket', 'Network/Http');
+
+/**
+ * Croogo Composer Wrapper
+ *
+ * @category Lib
+ * @package  Extension
+ * @since	 1.4
+ * @license  http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @link	 http://www.croogo.org
+ */
+class CroogoComposer {
+
+/**
+ * Path to APP
+ *
+ * @var string
+ */
+	public $appPath = APP;
+
+/**
+ * Downloads composer if it doesn't exist
+ *
+ * @return boolean
+ */
+	public function getComposer() {
+		if (!file_exists($this->appPath . 'composer.phar')) {
+			$this->_shellExec('curl -s http://getcomposer.org/installer | php -- --install-dir=' . $this->appPath);
+		}
+		return true;
+	}
+
+/**
+ * Runs composer.phar
+ *
+ * @return boolean
+ */
+	public function runComposer() {
+		$cmd = 'php ' . $this->appPath . 'composer.phar ';
+		if (file_exists($this->appPath . 'composer.lock')) {
+			$cmd .= 'update';
+		} else {
+			$cmd .= 'install';
+		}
+		$this->_shellExec($cmd);
+		return true;
+	}
+
+/**
+ * setConfig
+ *
+ * @param array $requires
+ * @return boolean
+ */
+	public function setConfig($requires = array()) {
+		$filename = 'composer.json';
+		if (file_exists($this->appPath . $filename)) {
+			$file = new File($this->appPath . $filename);
+			$json = json_decode($file->read(), true);
+		} else {
+			$file = new File($this->appPath . $filename, true);
+			$json = array();
+		}
+		if (!isset($json['require'])) {
+			$json['require'] = array('composer/installers' => '*');
+		}
+		foreach ($requires as $pkg => $ver) {
+			if (strpos($ver, '/') !== false) {
+				$pkg = $ver;
+				$ver = '*';
+			}
+			$json['require'][$pkg] = $ver;
+		}
+		$json = $this->_stringifyJson($json) . "\n";
+		$file->write($json);
+		$file->close();
+		return true;
+	}
+
+/**
+ * Returns an array in a pretty json format
+ *
+ * @param array $json
+ * @return string
+ * @author http://recursive-design.com/blog/2008/03/11/format-json-with-php/
+ */
+	protected function _stringifyJson($json = array()) {
+		$json = json_encode($json);
+		$json = str_replace(array('\/', ':{', ':"'), array('/', ': {', ': "'), $json);
+		$result			= '';
+		$pos			= 0;
+		$strLen			= strlen($json);
+		$indentStr		= "\t";
+		$newLine		= "\n";
+		$prevChar		= '';
+		$outOfQuotes	= true;
+		for ($i = 0; $i <= $strLen; $i++) {
+			$char = substr($json, $i, 1);
+			if ($char == '"' && $prevChar != '\\') {
+				$outOfQuotes = !$outOfQuotes;
+			} else if(($char == '}' || $char == ']') && $outOfQuotes) {
+				$result .= $newLine;
+				$pos --;
+				for ($j = 0; $j < $pos; $j++) {
+					$result .= $indentStr;
+				}
+			}
+			$result .= $char;
+			if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
+				$result .= $newLine;
+				if ($char == '{' || $char == '[') {
+					$pos ++;
+				}
+				for ($j = 0; $j < $pos; $j++) {
+					$result .= $indentStr;
+				}
+			}
+			$prevChar = $char;
+		}
+		return $result;
+	}
+
+/**
+ * Wrapper for shell_exec() method for testing
+ */
+	protected function _shellExec($cmd) {
+		return shell_exec($cmd);
+	}
+	
+}
---
+++ Plugin/Extensions/Test/Case/Lib/CroogoComposerTest.php
@@ -0,0 +1,109 @@
+<?php
+App::uses('CroogoComposer', 'Extensions.Lib');
+
+/**
+ * Croogo Composer Test
+ *
+ * PHP version 5
+ *
+ * @category Test
+ * @package  Croogo
+ * @version  1.4
+ * @author   Fahad Ibnay Heylaal <contact@fahad19.com>
+ * @license  http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @link     http://www.croogo.org
+ */
+class CroogoComposerTest extends CakeTestCase {
+	
+/**
+ * setUp
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+		App::build(array(
+			'Plugin' => array(CakePlugin::path('Extensions') . 'Test' . DS . 'test_app' . DS . 'Plugin' . DS),
+			'View' => array(CakePlugin::path('Extensions') . 'Test' . DS . 'test_app' . DS . 'View' . DS),
+		), App::PREPEND);
+		$this->testPlugin = CakePlugin::path('Extensions') . 'Test' . DS . 'test_files' . DS . 'example_plugin.zip';
+		$this->testTheme = CakePlugin::path('Extensions') . 'Test' . DS . 'test_files' . DS . 'example_theme.zip';
+		$this->CroogoComposer = new CroogoComposer();
+		$this->CroogoComposer->appPath = CakePlugin::path('Extensions') . 'Test' . DS . 'test_app' . DS;
+	}
+
+/**
+ * tearDown
+ *
+ * @return void
+ */
+	public function tearDown() {
+		parent::tearDown();
+		$path = CakePlugin::path('Extensions') . 'Test' . DS . 'test_app' . DS . 'Plugin' . DS . 'Example';
+		$Folder = new Folder($path);
+		$Folder->delete();
+		$path = CakePlugin::path('Extensions') . 'Test' . DS . 'test_app' . DS . 'View' . DS . 'Themed' . DS . 'Minimal';
+		$Folder = new Folder($path);
+		$Folder->delete();
+		$File = new File(CakePlugin::path('Extensions') . 'Test' . DS . 'test_app' . DS . 'composer.json');
+		$File->delete();
+	}
+
+/**
+ * testGetComposer
+ *
+ * @return void
+ */
+	public function testGetComposer() {
+		$CroogoComposer = $this->getMock('CroogoComposer', array('_shellExec'));
+		$CroogoComposer->appPath = $this->CroogoComposer->appPath;
+		$CroogoComposer->expects($this->once())
+			->method('_shellExec')
+			->with(
+				$this->equalTo('curl -s http://getcomposer.org/installer | php -- --install-dir=' . $CroogoComposer->appPath)
+			)
+			->will($this->returnValue(true));
+		$CroogoComposer->getComposer();
+	}
+
+/**
+ * testRunComposer
+ *
+ * @return void
+ */
+	public function testRunComposer() {
+		$CroogoComposer = $this->getMock('CroogoComposer', array('_shellExec'));
+		$CroogoComposer->appPath = $this->CroogoComposer->appPath;
+		$CroogoComposer->expects($this->once())
+			->method('_shellExec')
+			->with(
+				$this->equalTo('php ' . $CroogoComposer->appPath . 'composer.phar install')
+			)
+			->will($this->returnValue(true));
+		$CroogoComposer->runComposer();
+	}
+
+/**
+ * testSetConfig
+ *
+ * @return void
+ */
+	public function testSetConfig() {
+		$result = $this->CroogoComposer->setConfig(array(
+			'shama/ftp' => '*',
+		));
+		$this->assertTrue($result);
+		$File = new File($this->CroogoComposer->appPath . 'composer.json');
+		$result = $File->read();
+		$File->close();
+		$expected = <<<END
+{
+	"require": {
+		"composer/installers": "*",
+		"shama/ftp": "*"
+	}
+}
+END;
+		$this->assertEquals(trim($expected), trim($result));
+	}
+}
