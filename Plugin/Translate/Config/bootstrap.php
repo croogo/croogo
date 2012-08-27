ead6e23136eca30af1870d5075a823b67bb1c399
Marek Sebera <marek.sebera@gmail.com>
2012-08-27 17:28:51 +0300

    Update Plugin/Translate/Config/bootstrap.php

--- Plugin/Translate/Config/bootstrap.php
+++ Plugin/Translate/Config/bootstrap.php
@@ -1,29 +1,39 @@
 <?php
+
+function getModelAlias($modelAlias = null) {
+    if ($modelAlias == null) {
+        throw new Exception('modelAlias cannot be null');
+    }
+    $alias = explode('.', $modelAlias);
+    return $alias[count($alias) - 1];
+}
+
 /**
  * Configuration
  *
  */
 Configure::write('Translate.models', array(
-	'Node' => array(
-		'title' => 'titleTranslation',
-		'excerpt' => 'excerptTranslation',
-		'body' => 'bodyTranslation',
-	),
-	'Block' => array(
-		'title' => 'titleTranslation',
-		'body' => 'bodyTranslation',
-	),
-	'Link' => array(
-		'title' => 'titleTranslation',
-		'description' => 'descriptionTranslation',
-	),
+    'Node' => array(
+        'title' => 'titleTranslation',
+        'excerpt' => 'excerptTranslation',
+        'body' => 'bodyTranslation',
+    ),
+    'Block' => array(
+        'title' => 'titleTranslation',
+        'body' => 'bodyTranslation',
+    ),
+    'Link' => array(
+        'title' => 'titleTranslation',
+        'description' => 'descriptionTranslation',
+    )
 ));
 
 /**
+ * 
  * Do not edit below this line unless you know what you are doing.
  *
  */
 foreach (Configure::read('Translate.models') as $translateModel => $fields) {
-	Croogo::hookBehavior($translateModel, 'CroogoTranslate', $fields);
-	Croogo::hookAdminRowAction(Inflector::pluralize($translateModel) . '/admin_index', 'Translate', 'plugin:translate/controller:translate/action:index/:id/' . $translateModel);
+    Croogo::hookBehavior(getModelAlias($translateModel), 'CroogoTranslate', $fields);
+    Croogo::hookAdminRowAction(Inflector::pluralize(getModelAlias($translateModel)) . '/admin_index', __('Translate'), 'plugin:translate/controller:translate/action:index/:id/' . $translateModel);
 }
