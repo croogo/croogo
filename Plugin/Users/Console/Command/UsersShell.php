51627f38717e2be0903d5589be0f585c1052fa6e
Rachman Chavik <rchavik@xintesa.com>
2013-02-05 14:42:38 +0700

    adding getOptionParser for Users.Users shell

--- Plugin/Users/Console/Command/UsersShell.php
+++ Plugin/Users/Console/Command/UsersShell.php
@@ -6,15 +6,44 @@ class UsersShell extends AppShell {
 		'Users.User',
 	);
 
+/**
+ * getOptionParser
+ */
+	public function getOptionParser() {
+		return parent::getOptionParser()
+			->addSubCommand('reset', array(
+				'help' => __('Reset user password'),
+				'parser' => array(
+					'arguments' => array(
+						'username' => array(
+							'required' => true,
+							'help' => __('Username to reset'),
+						),
+						'password' => array(
+							'required' => true,
+							'help' => __('New user password'),
+						),
+					),
+				),
+			))
+			;
+	}
+
+/**
+ * reset
+ */
 	public function reset() {
 		$username = $this->args[0];
 		$password = $this->args[1];
 
 		$user = $this->User->findByUsername($username);
 		if (empty($user['User']['id'])) {
-			return $this->err(__('User %s not found', $username));
+			return $this->warn(__('User \'%s\' not found', $username));
 		}
 		$this->User->id = $user['User']['id'];
-		$this->User->saveField('password', $password);
+		$result = $this->User->saveField('password', $password);
+		if ($result) {
+			$this->success(__('Password for \'%s\' has been changed', $username));
+		}
 	}
 }
