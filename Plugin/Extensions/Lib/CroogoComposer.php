d515492af088b62b12ff92d190e9facd9aaf7319
Rachman Chavik <rchavik@xintesa.com>
2013-03-21 12:38:14 +0700

    docblocks: missing @license, @author, alignment

--- Plugin/Extensions/Lib/CroogoComposer.php
+++ Plugin/Extensions/Lib/CroogoComposer.php
@@ -9,9 +9,10 @@ App::uses('CroogoJson', 'Lib');
  *
  * @category Lib
  * @package  Extension
- * @since	 1.4
+ * @since    1.4
+ * @author   Kyle Robinson Young <kyle@dontkry.com>
  * @license  http://www.opensource.org/licenses/mit-license.php The MIT License
- * @link	 http://www.croogo.org
+ * @link     http://www.croogo.org
  */
 class CroogoComposer {
 
