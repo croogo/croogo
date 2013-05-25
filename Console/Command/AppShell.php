9fc02c69b8656b14fd69f93a46934a1fa28a126b
Rachman Chavik <rchavik@xintesa.com>
2013-05-25 12:02:43 +0700

    Croogo: Move methods from AppShell to CroogoAppShell

--- Console/Command/AppShell.php
+++ Console/Command/AppShell.php
@@ -1,28 +1,7 @@
 <?php
 
-App::uses('Shell', 'Console');
+App::uses('CroogoAppShell', 'Croogo.Console');
 
-class AppShell extends Shell {
-
-/**
- * Convenience method for out() that encloses message between <info /> tag
- */
-	public function info($message = null, $newlines = 1, $level = Shell::NORMAL) {
-		$this->out('<info>' . $message . '</info>', $newlines, $level);
-	}
-
-/**
- * Convenience method for out() that encloses message between <warning /> tag
- */
-	public function warn($message = null, $newlines = 1, $level = Shell::NORMAL) {
-		$this->out('<warning>' . $message . '</warning>', $newlines, $level);
-	}
-
-/**
- * Convenience method for out() that encloses message between <success /> tag
- */
-	public function success($message = null, $newlines = 1, $level = Shell::NORMAL) {
-		$this->out('<success>' . $message . '</success>', $newlines, $level);
-	}
+class AppShell extends CroogoAppShell {
 
 }
