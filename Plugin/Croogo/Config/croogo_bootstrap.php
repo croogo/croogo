261ddc4bf66b339f9f960504dbfb434b76f7d0bb
Rachman Chavik <rchavik@xintesa.com>
2013-08-29 10:56:34 +0700

    Fix: Running SchemaShell throws missing croogo_app_models table
    Adding Croogo.Model directory in the search path causes Cake to
    assume that CroogoAppModel is a real model with physical table.
    Fix this by remove some Croogo plugin paths from the default
    package search path.
    
    These paths were originally added to support backward compatibility to
    allow commands, existing layout/views to be referenced as before. So,
    the Console/Command, View, and View/Helper paths are kept.

--- Plugin/Croogo/Config/croogo_bootstrap.php
+++ Plugin/Croogo/Config/croogo_bootstrap.php
@@ -43,11 +43,6 @@ Configure::write('Config.language', Configure::read('Site.locale'));
 $croogoPath = CakePlugin::path('Croogo');
 App::build(array(
 	'Console/Command' => array($croogoPath . 'Console' . DS . 'Command' . DS),
-	'Controller' => array($croogoPath . 'Controller' . DS),
-	'Controller/Component' => array($croogoPath . 'Controller' . DS . 'Component' . DS),
-	'Lib' => array($croogoPath . 'Lib' . DS),
-	'Model' => array($croogoPath . 'Model' . DS),
-	'Model/Behavior' => array($croogoPath . 'Model' . DS . 'Behavior' . DS),
 	'View' => array($croogoPath . 'View' . DS),
 	'View/Helper' => array($croogoPath . 'View' . DS . 'Helper' . DS),
 ), App::APPEND);
