croogo/croogo

commit c12bf0aa1e234ad025c13b3bdad0cd9b7c7ac564

reduce the amount of loop and use native functions
Closes GH-111

Signed-off-by: Rachman Chavik <rchavik@xintesa.com>

Plugin/Extensions/Lib/ExtensionsInstaller.php
@@ -65,20 +65,31 @@ class ExtensionsInstaller {
 		}
 		$Zip = new ZipArchive;
 		if ($Zip->open($path) === true) {
-			$searches = array(
-				'Config*Activation',
-				'Controller*AppController',
-				'Model*AppModel',
-				'View*AppHelper',
-			);
-			for ($i = 0; $i < $Zip->numFiles; $i++) {
-				$file = $Zip->getNameIndex($i);
+			$search = 'Config/plugin.json';
+			$indexJson = $Zip->locateName('plugin.json', ZIPARCHIVE::FL_NODIR);
+			if ($indexJson === FALSE) {
+				throw new CakeException(__('Invalid zip archive'));
+			} else {
+				$fileName = $Zip->getNameIndex($indexJson);
+				$fileJson = json_decode($Zip->getFromIndex($indexJson));
+				
+				if (empty($fileJson->name)) {
+					throw new CakeException(__('Invalid plugin'));
+				}
+				
+				$this->_rootPath[$path] = str_replace($search, '', $fileName);
+				$plugin = $fileJson->name;
+				
+				$searches = array(
+					$plugin . 'Activation.php',
+					$plugin . 'AppController.php',
+					$plugin . 'AppModel.php',
+					$plugin . 'Helper.php'
+				);
+				
 				foreach ($searches as $search) {
-					$search = str_replace('*', '\/([\w]+)', $search);
-					if (preg_match('/' . $search . '\.php/', $file, $matches)) {
-						$plugin = trim($matches[1]);
-						$this->_rootPath[$path] = str_replace($matches[0], '', $file);
-						break 2;
+					if ($Zip->locateName($search, ZIPARCHIVE::FL_NODIR) === FALSE) {
+						throw new CakeException(__('Invalid plugin'));
 					}
 				}
 			}

commit 156caba78e73d7184ca12908a69a4325e2777979

Fix: Valid plugins are marked as Invalid
This is a regression introduced in c12bf0aa1e2 where it requires all
files in $searches to be present. Instead, only mark it as invalid
when none of those files can be found.

Reported-By: Mohammad Saleh
Ref: http://thread.gmane.org/gmane.comp.cms.croogo/191

Plugin/Extensions/Lib/ExtensionsInstaller.php
@@ -75,7 +75,7 @@ class ExtensionsInstaller {
 				$fileJson = json_decode($Zip->getFromIndex($indexJson));
 
 				if (empty($fileJson->name)) {
-					throw new CakeException(__d('croogo', 'Invalid plugin'));
+					throw new CakeException(__d('croogo', 'Invalid plugin.json or missing plugin name'));
 				}
 
 				$this->_rootPath[$path] = str_replace($search, '', $fileName);
@@ -88,10 +88,16 @@ class ExtensionsInstaller {
 					$plugin . 'Helper.php'
 				);
 
+				$hasFile = false;
 				foreach ($searches as $search) {
-					if ($Zip->locateName($search, ZIPARCHIVE::FL_NODIR) === false) {
-						throw new CakeException(__d('croogo', 'Invalid plugin'));
+					if ($Zip->locateName($search, ZIPARCHIVE::FL_NODIR) !== false) {
+						$hasFile = true;
+						break;
 					}
+				}
+				if (!$hasFile) {
+					CakeLog::critical(__d('croogo', 'Missing expected files: %s in: %s', implode(',', $searches), $path));
+					throw new CakeException(__d('croogo', 'Invalid plugin or missing expected files'));
 				}
 			}
 			$Zip->close();

Plugin/Extensions/Test/Case/Lib/ExtensionsInstallerTest.php
@@ -25,11 +25,14 @@ class ExtensionsInstallerTest extends CroogoTestCase {
  */
 	public function setUp() {
 		parent::setUp();
+		$path = CakePlugin::path('Extensions') . 'Test' . DS;
 		App::build(array(
 			'Plugin' => array(CakePlugin::path('Extensions') . 'Test' . DS . 'test_app' . DS . 'Plugin' . DS),
 			'View' => array(CakePlugin::path('Extensions') . 'Test' . DS . 'test_app' . DS . 'View' . DS),
 		), App::PREPEND);
 		$this->testPlugin = CakePlugin::path('Extensions') . 'Test' . DS . 'test_files' . DS . 'example_plugin.zip';
+		$this->minimalPlugin = $path . 'test_files' . DS . 'minimal_plugin.zip';
+		$this->invalidPlugin = $path . 'test_files' . DS . 'invalid_plugin.zip';
 		$this->testTheme = CakePlugin::path('Extensions') . 'Test' . DS . 'test_files' . DS . 'example_theme.zip';
 		$this->ExtensionsInstaller = new ExtensionsInstaller();
 	}
@@ -47,6 +50,42 @@ class ExtensionsInstallerTest extends CroogoTestCase {
 		$path = CakePlugin::path('Extensions') . 'Test' . DS . 'test_app' . DS . 'View' . DS . 'Themed' . DS . 'Minimal';
 		$Folder = new Folder($path);
 		$Folder->delete();
+		if (file_exists($this->minimalPlugin)) {
+			unlink($this->minimalPlugin);
+		}
+		if (file_exists($this->invalidPlugin)) {
+			unlink($this->invalidPlugin);
+		}
+	}
+
+/**
+ * Helper method to create test zip file
+ */
+	protected function _addDirectoryToZip($zip, $dir, $base) {
+		$newFolder = str_replace($base, '', $dir);
+		$zip->addEmptyDir($newFolder);
+		foreach (glob($dir . '/*') as $file) {
+			if (is_dir($file)) {
+				$zip = $this->_addDirectoryToZip($zip, $file, $base);
+			} else {
+				$newFile = str_replace($base, '', $file);
+				$zip->addFile($file, $newFile);
+			}
+		}
+		return $zip;
+	}
+
+/**
+ * Create a test zip file $zipPath from $dirName
+ */
+	protected function _createZip($zipPath, $dirName) {
+		$dir = CakePlugin::path('Extensions') . 'Test' . DS . 'test_files' . DS;
+		chdir($dir);
+		$zip = new ZipArchive();
+		$zip->open($zipPath, ZipArchive::OVERWRITE);
+		$this->_addDirectoryToZip($zip, $dirName, $dir);
+		$zip->close();
+		$this->assertTrue(file_exists($zipPath), 'Test zip not created');
 	}
 
 /**
@@ -57,6 +96,28 @@ class ExtensionsInstallerTest extends CroogoTestCase {
 	public function testGetPluginName() {
 		$result = $this->ExtensionsInstaller->getPluginName($this->testPlugin);
 		$this->assertEquals('Example', $result);
+	}
+
+/**
+ * testGetPluginName
+ *
+ * @return void
+ */
+	public function testGetPluginNameMinimal() {
+		$this->_createZip($this->minimalPlugin, 'Minimal');
+		$result = $this->ExtensionsInstaller->getPluginName($this->minimalPlugin);
+		$this->assertEquals('Minimal', $result);
+	}
+
+/**
+ * testGetPluginNameInvalid
+ *
+ * @return void
+ * @expectedException CakeException
+ */
+	public function testGetPluginNameInvalid() {
+		$this->_createZip($this->invalidPlugin, 'Invalid');
+		$result = $this->ExtensionsInstaller->getPluginName($this->invalidPlugin);
 	}
 
 /**

Plugin/Extensions/Test/test_files/Minimal/Controller/MinimalAppController.php
@@ -1,0 +1,4 @@
+<?php
+App::uses('AppController', 'Controller');
+class MinimalAppController extends AppController {
+}