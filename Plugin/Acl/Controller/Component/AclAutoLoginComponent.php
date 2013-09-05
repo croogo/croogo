28f639d51e2974b5f36714fd733a7920c64a5421
Rachman Chavik <rchavik@xintesa.com>
2013-09-05 11:09:32 +0700

    CookieAuthenticate: Implementation error
    - Update the CookieAuthenticate class from upstream
    - AutoLoginComponent was doing all the work instead of reusing
      CookieAuthenticate. Fix it by removing those and move cookie
      verification into the Authenticate class.
    - Use hash_hmac() against cookie payload

--- Plugin/Acl/Controller/Component/AclAutoLoginComponent.php
+++ Plugin/Acl/Controller/Component/AclAutoLoginComponent.php
@@ -76,12 +76,6 @@ class AclAutoLoginComponent extends Component {
 			array($this, 'onAdminLoginSuccessful'),
 			'Controller.Users.adminLoginSuccessful'
 		);
-
-		$skipActions = array('logout', 'admin_logout');
-		if (!in_array($controller->request->params['action'], $skipActions) &&
-			empty($controller->request->data)) {
-			$this->_loginByCookie();
-		}
 	}
 
 /**
@@ -92,10 +86,14 @@ class AclAutoLoginComponent extends Component {
 	protected function _cookie($request) {
 		$time = time();
 		$username = $request->data[$this->_userModel][$this->_fields['username']];
-		return array_merge(array(
+		$data = json_encode(array(
 			'hash' => $this->Auth->password($username . $time),
 			'time' => $time,
-		), $request->data);
+			'username' => $username,
+		));
+
+		$mac = hash_hmac('sha256', $data, Configure::read('Security.salt'));
+		return compact('mac', 'data');
 	}
 
 /**
@@ -140,50 +138,6 @@ class AclAutoLoginComponent extends Component {
 	protected function _deleteCookie($key) {
 		$this->Cookie->name = $this->settings['cookieName'];
 		$this->Cookie->delete($key);
-	}
-
-/**
- * Verify cookie data
- *
- * return array|boolean false when data is invalid
- */
-	protected function _verify() {
-		if (!$this->_checkCookie($this->_userModel)) {
-			return array();
-		}
-		$data = $this->_readCookie($this->_userModel);
-		if (is_array($data) && !empty($data['time'])) {
-			$hash = $data['hash'];
-			$time = $data['time'];
-			$username = $data[$this->_userModel][$this->_fields['username']];
-			if ($hash === $this->Auth->password($username . $time)) {
-				return $data;
-			}
-		}
-		return false;
-	}
-
-/**
- * Automatically login when cookie exists and valid
- *
- * @return void
- * @throws UnauthorizedException
- */
-	protected function _loginByCookie() {
-		$Controller = $this->_Controller;
-		$request = $Controller->request;
-		$userId = $this->Auth->user('id');
-		if (empty($userId)) {
-			$request->data = $this->_verify();
-			if ($request->data === false) {
-				$this->_deleteCookie($this->_userModel);
-				throw new UnauthorizedException('Invalid cookie');
-			}
-			if ($this->Auth->login()) {
-				$Controller->Session->delete('Message.auth');
-				return $Controller->redirect($this->Auth->redirectUrl());
-			}
-		}
 	}
 
 /**
