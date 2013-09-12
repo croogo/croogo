croogo/croogo

commit fa8ec5f216ed040e899fb6cb21b8e0efb750dcbc

Fix: Public menu items are not shown
Public users have no Auth.User data in session. Instead, reuse the value
from CroogoComponent::roleId initialized in CroogoComponent::startup().

Reported-by: sebastian
Ref: http://thread.gmane.org/gmane.comp.cms.croogo/168

Plugin/Blocks/Controller/Component/BlocksComponent.php
@@ -84,7 +84,7 @@ class BlocksComponent extends Component {
 				'config' => 'croogo_blocks',
 			),
 		));
-		$roleId = $this->controller->Auth->user('role_id');
+		$roleId = $this->controller->Croogo->roleId;
 		foreach ($regions as $regionId => $regionAlias) {
 			$this->blocksForLayout[$regionAlias] = array();
 			$findOptions = array(

Plugin/Menus/Controller/Component/MenusComponent.php
@@ -88,7 +88,7 @@ class MenusComponent extends Component {
 		}
 		$menus = Hash::merge($menus, array_keys($this->controller->Blocks->blocksData['menus']));
 
-		$roleId = $this->controller->Auth->user('role_id');
+		$roleId = $this->controller->Croogo->roleId;
 		foreach ($menus as $menuAlias) {
 			$menu = $this->Link->Menu->find('first', array(
 				'conditions' => array(

Plugin/Menus/Test/test_app/View/MenusTest/index.ctp
@@ -1,0 +1,1 @@
+index

Plugin/Menus/Test/Case/Controller/Component/MenusComponentTest.php
@@ -1,0 +1,86 @@
+<?php
+
+App::uses('Controller', 'Controller');
+App::uses('CroogoControllerTestCase', 'Croogo.TestSuite');
+
+App::uses('Controller', 'Controller');
+
+class MenusTestController extends Controller {
+
+	public $components = array(
+		'Auth',
+		'Session',
+		'Croogo.Croogo',
+		'Blocks.Blocks',
+		'Menus.Menus',
+	);
+
+	public function beforeFilter() {
+		$this->Auth->allow('index');
+		parent::beforeFilter();
+	}
+
+	public function index() {
+	}
+
+}
+
+class MenusComponentTest extends CroogoControllerTestCase {
+
+	public $fixtures = array(
+		'plugin.blocks.block',
+		'plugin.blocks.region',
+		'plugin.menus.menu',
+		'plugin.menus.link',
+	);
+
+	public function setUp() {
+		$this->_paths = App::paths();
+		$app = CakePlugin::path('Menus') . 'Test' . DS . 'test_app' . DS;
+		App::build(array(
+			'Controller' => array(
+				$app . 'Controller' . DS,
+			),
+			'View' => array(
+				$app . 'View' . DS,
+			),
+		));
+		$this->generate('MenusTest');
+	}
+
+	public function tearDown() {
+		App::paths($this->_paths);
+		unset($this->controller);
+	}
+
+/**
+ * test that public Links are displayed
+ */
+	public function testMenuGenerationForPublic() {
+		$vars = $this->testAction('/index', array(
+			'return' => 'vars',
+		));
+		$result = Hash::extract(
+			$vars['menus_for_layout'],
+			'footer.threaded.{n}.Link[title=Public Link Only]'
+		);
+		$this->assertNotEmpty($result);
+	}
+
+/**
+ * test that public Links are not displayed
+ */
+	public function testMenuGenerationForRegistered() {
+		$this->controller->Session->write('Auth.User', array('id' => 3, 'role_id' => 2));
+		$vars = $this->testAction('/index', array(
+			'return' => 'vars',
+		));
+		$result = Hash::extract(
+			$vars['menus_for_layout'],
+			'footer.threaded.{n}.Link[title=Public Link Only]'
+		);
+		$this->assertEmpty($result);
+		$this->controller->Session->delete('Auth');
+	}
+
+}

Plugin/Menus/Test/Fixture/LinkFixture.php
@@ -242,5 +242,23 @@ class LinkFixture extends CroogoTestFixture {
 			'updated' => '2009-10-27 17:46:54',
 			'created' => '2009-10-27 17:46:54'
 		),
+		array(
+			'id' => 18,
+			'parent_id' => null,
+			'menu_id' => 4,
+			'title' => 'Public Link Only',
+			'class' => 'public-link-only',
+			'description' => '',
+			'link' => '/public-link-only',
+			'target' => '',
+			'rel' => '',
+			'status' => 1,
+			'lft' => 7,
+			'rght' => 8,
+			'visibility_roles' => '["3"]',
+			'params' => '',
+			'updated' => '2009-10-27 17:46:54',
+			'created' => '2009-10-27 17:46:54'
+		),
 	);
 }

Plugin/Menus/Test/test_app/View/Layouts/default.ctp
@@ -1,0 +1,2 @@
+<?php
+echo $content_for_layout;

commit 893ecc30bef652042b5c25309d7fc860e951a7e4

Blocks: visibility_roles was not correctly honoured
This is a regression introduced in fa8ec5f216.

BlocksComponent retrieves the roleId of the current user from
CroogoComponent::$roleId.  Since Blocks and Menus component runs on
on higher priority, CroogoComponent::startup() has not been executed
yet, leaving the default value being used.

Add session checks in Blocks and Menus Components and set the value to fix it.

- To properly fix this problem, `CroogoComponent::roleId()` method will be
  introduced in 1.6
- The property `CroogoComponent::$roleId` is deprecated in 1.5.x

Reported-By: Mohammad Saleh
Closes: #423

Plugin/Blocks/Controller/Component/BlocksComponent.php
@@ -84,6 +84,10 @@ class BlocksComponent extends Component {
 				'config' => 'croogo_blocks',
 			),
 		));
+
+		if ($this->controller->Session->check('Auth.User.role_id')) {
+			$this->controller->Croogo->roleId = $this->controller->Auth->user('role_id');
+		}
 		$roleId = $this->controller->Croogo->roleId;
 		foreach ($regions as $regionId => $regionAlias) {
 			$this->blocksForLayout[$regionAlias] = array();

Plugin/Croogo/Controller/Component/CroogoComponent.php
@@ -36,6 +36,7 @@ class CroogoComponent extends Component {
  *
  * @var integer
  * @access public
+ * @deprecated Will be removed in 1.6 and replaced with an API
  */
 	public $roleId = 3;
 

Plugin/Menus/Controller/Component/MenusComponent.php
@@ -88,6 +88,9 @@ class MenusComponent extends Component {
 		}
 		$menus = Hash::merge($menus, array_keys($this->controller->Blocks->blocksData['menus']));
 
+		if ($this->controller->Session->check('Auth.User.role_id')) {
+			$this->controller->Croogo->roleId = $this->controller->Auth->user('role_id');
+		}
 		$roleId = $this->controller->Croogo->roleId;
 		foreach ($menus as $menuAlias) {
 			$menu = $this->Link->Menu->find('first', array(

Plugin/Nodes/Controller/Component/NodesComponent.php
@@ -70,6 +70,11 @@ class NodesComponent extends Component {
  * @return void
  */
 	public function nodes() {
+		if ($this->controller->Session->check('Auth.User.role_id')) {
+			$this->controller->Croogo->roleId = $this->controller->Auth->user('role_id');
+		}
+		$roleId = $this->controller->Croogo->roleId;
+
 		$nodes = $this->controller->Blocks->blocksData['nodes'];
 		$_nodeOptions = array(
 			'find' => 'all',
@@ -77,7 +82,7 @@ class NodesComponent extends Component {
 				'Node.status' => 1,
 				'OR' => array(
 					'Node.visibility_roles' => '',
-					'Node.visibility_roles LIKE' => '%"' . $this->roleId . '"%',
+					'Node.visibility_roles LIKE' => '%"' . $roleId . '"%',
 				),
 			),
 			'order' => 'Node.created DESC',

Plugin/Nodes/Controller/NodesController.php
@@ -519,11 +519,16 @@ class NodesController extends NodesAppController {
 	public function promoted() {
 		$this->set('title_for_layout', __d('croogo', 'Nodes'));
 
+		$roleId = $this->Auth->user('role_id');
+		if (empty($roleId)) {
+			$roleId = $this->Croogo->roleId;
+		}
+
 		$this->paginate['Node']['type'] = 'promoted';
 		$this->paginate['Node']['conditions'] = array(
 			'OR' => array(
 				'Node.visibility_roles' => '',
-				'Node.visibility_roles LIKE' => '%"' . $this->Croogo->roleId . '"%',
+				'Node.visibility_roles LIKE' => '%"' . $roleId . '"%',
 			),
 		);
 

Plugin/Nodes/Model/Node.php
@@ -579,7 +579,7 @@ class Node extends NodesAppModel {
 	protected function _mergeQueryFilters(&$query, $key, $values) {
 		if (!empty($query[$key])) {
 			if (is_array($query[$key])) {
-				$query[$key] = array_merge($query[$key], $values);
+				$query[$key] = Hash::merge($query[$key], $values);
 			}
 		} else {
 			$query[$key] = $values;

Plugin/Blocks/Test/Case/Controller/BlocksControllerTest.php
@@ -175,6 +175,8 @@ class BlocksControllerTest extends CroogoControllerTestCase {
 			'blogroll',
 			'recent_posts',
 			'meta',
+			'block-visible-by-public',
+			'block-visible-by-admin-or-registered',
 		));
 	}
 
@@ -201,6 +203,8 @@ class BlocksControllerTest extends CroogoControllerTestCase {
 			'categories',
 			'recent_posts',
 			'meta',
+			'block-visible-by-public',
+			'block-visible-by-admin-or-registered',
 		));
 	}
 
@@ -227,6 +231,8 @@ class BlocksControllerTest extends CroogoControllerTestCase {
 			'blogroll',
 			'recent_posts',
 			'meta',
+			'block-visible-by-public',
+			'block-visible-by-admin-or-registered',
 		));
 	}
 
@@ -253,6 +259,8 @@ class BlocksControllerTest extends CroogoControllerTestCase {
 			'search',
 			'recent_posts',
 			'meta',
+			'block-visible-by-public',
+			'block-visible-by-admin-or-registered',
 		));
 	}
 
@@ -288,6 +296,8 @@ class BlocksControllerTest extends CroogoControllerTestCase {
 			'search',
 			'categories',
 			'recent_posts',
+			'block-visible-by-public',
+			'block-visible-by-admin-or-registered',
 		));
 	}
 
@@ -344,6 +354,8 @@ class BlocksControllerTest extends CroogoControllerTestCase {
 			'blogroll',
 			'recent_posts',
 			'meta',
+			'block-visible-by-public',
+			'block-visible-by-admin-or-registered',
 		));
 	}
 
@@ -382,6 +394,8 @@ class BlocksControllerTest extends CroogoControllerTestCase {
 		$this->assertEqual($blockAliases, array(
 			'categories',
 			'recent_posts',
+			'block-visible-by-public',
+			'block-visible-by-admin-or-registered',
 		));
 	}
 

Plugin/Blocks/Test/Case/Controller/Component/BlocksComponentTest.php
@@ -1,0 +1,125 @@
+<?php
+
+App::uses('Controller', 'Controller');
+App::uses('CroogoControllerTestCase', 'Croogo.TestSuite');
+
+App::uses('Controller', 'Controller');
+
+class BlocksTestController extends Controller {
+
+	public $components = array(
+		'Auth',
+		'Session',
+		'Croogo.Croogo',
+		'Blocks.Blocks',
+	);
+
+	public function beforeFilter() {
+		$this->Auth->allow('index');
+		parent::beforeFilter();
+	}
+
+	public function index() {
+	}
+
+}
+
+class BlocksComponentTest extends CroogoControllerTestCase {
+
+	public $fixtures = array(
+		'plugin.blocks.block',
+		'plugin.blocks.region',
+		'plugin.menus.menu',
+		'plugin.menus.link',
+	);
+
+	public function setUp() {
+		$this->_paths = App::paths();
+		$app = CakePlugin::path('Blocks') . 'Test' . DS . 'test_app' . DS;
+		App::build(array(
+			'Controller' => array(
+				$app . 'Controller' . DS,
+			),
+			'View' => array(
+				$app . 'View' . DS,
+			),
+		));
+		$this->generate('BlocksTest');
+	}
+
+	public function tearDown() {
+		App::paths($this->_paths);
+		unset($this->controller);
+	}
+
+/**
+ * test that public Blocks are displayed
+ */
+	public function testBlockGenerationForPublic() {
+		$this->controller->layout = 'ajax';
+		$vars = $this->testAction('/index', array(
+			'return' => 'vars',
+		));
+
+		$result = Hash::extract(
+			$vars['blocks_for_layout'],
+			'right.{n}.Block[title=Block Visible by Admin or Registered]'
+		);
+		$this->assertEmpty($result);
+
+		$result = Hash::extract(
+			$vars['blocks_for_layout'],
+			'right.{n}.Block[title=Block Visible by Public]'
+		);
+		$this->assertNotEmpty($result);
+	}
+
+/**
+ * test that block are displayed for Registered
+ */
+	public function testBlockGenerationForRegistered() {
+		$this->controller->layout = 'ajax';
+		$this->controller->Session->write('Auth.User', array('id' => 3, 'role_id' => 2));
+		$vars = $this->testAction('/index', array(
+			'return' => 'vars',
+		));
+
+		$result = Hash::extract(
+			$vars['blocks_for_layout'],
+			'right.{n}.Block[title=Block Visible by Public]'
+		);
+		$this->assertEmpty($result);
+
+		$result = Hash::extract(
+			$vars['blocks_for_layout'],
+			'right.{n}.Block[title=Block Visible by Admin or Registered]'
+		);
+		$this->assertNotEmpty($result);
+		$this->controller->Session->delete('Auth');
+	}
+
+/**
+ * test that block are displayed for Admin
+ */
+	public function testBlockGenerationForAdmin() {
+		$this->controller->layout = 'ajax';
+		$this->controller->Session->write('Auth.User', array('id' => 1, 'role_id' => 1));
+		$vars = $this->testAction('/index', array(
+			'return' => 'vars',
+		));
+
+		$result = Hash::extract(
+			$vars['blocks_for_layout'],
+			'right.{n}.Block[title=Block Visible by Public]'
+		);
+		$this->assertEmpty($result);
+
+		$result = Hash::extract(
+			$vars['blocks_for_layout'],
+			'right.{n}.Block[title=Block Visible by Admin or Registered]'
+		);
+		$this->assertNotEmpty($result);
+		$this->controller->Session->delete('Auth');
+	}
+
+}

Plugin/Blocks/Test/Fixture/BlockFixture.php
@@ -134,5 +134,41 @@ class BlockFixture extends CroogoTestFixture {
 			'updated' => '2010-04-08 21:09:31',
 			'created' => '2009-12-22 05:17:32'
 		),
+		array(
+			'id' => 10,
+			'region_id' => 4,
+			'title' => 'Block Visible by Public',
+			'alias' => 'block-visible-by-public',
+			'body' => 'Block Visible by Public',
+			'show_title' => 1,
+			'class' => '',
+			'status' => 1,
+			'weight' => 7,
+			'element' => '',
+			'visibility_roles' => '["3"]',
+			'visibility_paths' => '',
+			'visibility_php' => '',
+			'params' => '',
+			'updated' => '2010-04-08 21:09:31',
+			'created' => '2009-12-22 05:17:32'
+		),
+		array(
+			'id' => 11,
+			'region_id' => 4,
+			'title' => 'Block Visible by Admin or Registered',
+			'alias' => 'block-visible-by-admin-or-registered',
+			'body' => 'Block Visible by Admin or Registered',
+			'show_title' => 1,
+			'class' => '',
+			'status' => 1,
+			'weight' => 8,
+			'element' => '',
+			'visibility_roles' => '["1","2"]',
+			'visibility_paths' => '',
+			'visibility_php' => '',
+			'params' => '',
+			'updated' => '2010-04-08 21:09:31',
+			'created' => '2009-12-22 05:17:32'
+		),
 	);
 }

Plugin/Blocks/Test/test_app/View/BlocksTest/index.ctp
@@ -1,0 +1,1 @@
+index

Plugin/Nodes/Test/Case/Controller/NodesControllerTest.php
@@ -81,15 +81,20 @@ class NodesControllerTest extends CroogoControllerTestCase {
 			),
 			'components' => array(
 				'Auth' => array('user'),
-				'Session',
+				'Session' => array('read', 'setFlash'),
 				'Security' => array('blackHole'),
 			),
 		));
+
 		$this->NodesController->Node->Behaviors->detach('Acl');
 		$this->NodesController->Auth
 			->staticExpects($this->any())
 			->method('user')
 			->will($this->returnCallback(array($this, 'authUserCallback')));
+		$this->NodesController->Session
+			->staticExpects($this->any())
+			->method('read')
+			->will($this->returnValue(array('id' => 1, 'role_id' => 1)));
 		$this->NodesController->Security->Session = $this->getMock('CakeSession');
 	}
 
@@ -111,10 +116,40 @@ class NodesControllerTest extends CroogoControllerTestCase {
 	public function testAdminIndex() {
 		$this->testAction('/admin/nodes/index');
 		$this->assertNotEmpty($this->vars['nodes']);
-		$this->assertEquals(2, count($this->vars['nodes']));
+		$this->assertEquals(3, count($this->vars['nodes']));
 		$this->assertNotEmpty($this->vars['nodes'][0]['Node']);
 		$this->assertNotEmpty($this->vars['nodes'][0]['User']);
 		$this->assertArrayHasKey('CustomFields', $this->vars['nodes'][0]);
+	}
+
+/**
+ * testPromotedWithVisibilityRole
+ *
+ * @return void
+ */
+	public function testPromotedWithVisibilityRole() {
+		CakeSession::write('Auth.User', array(
+			'id' => 1,
+			'role_id' => 1,
+		));
+		$this->testAction('/nodes/nodes/promoted');
+		$this->assertTrue(count($this->vars['nodes']) === 2);
+	}
+
+/**
+ * testIndexWithVisibilityRole
+ *
+ * @return void
+ */
+	public function testIndexWithVisibilityRole() {
+		CakeSession::write('Auth.User', array(
+			'id' => 1,
+			'role_id' => 1,
+		));
+		$this->testAction('/nodes/nodes/index/type:page', array(
+			'return' => 'vars',
+		));
+		$this->assertTrue(count($this->vars['nodes']) === 2);
 	}
 
 /**
@@ -138,10 +173,12 @@ class NodesControllerTest extends CroogoControllerTestCase {
 		$this->testAction('/admin/nodes/nodes/index/links:1/filter:about');
 		$this->assertEquals('admin_popup', $this->controller->View->layout);
 		$this->assertNotEmpty($this->vars['nodes']);
+
+		$about = $this->vars['nodes'][1];
 		$this->assertNotEmpty($this->vars['nodes'][0]['Node']);
-		$this->assertEquals('about', $this->vars['nodes'][0]['Node']['slug']);
-		$this->assertNotEmpty($this->vars['nodes'][0]['User']);
-		$this->assertArrayHasKey('CustomFields', $this->vars['nodes'][0]);
+		$this->assertEquals('about', $about['Node']['slug']);
+		$this->assertNotEmpty($about['User']);
+		$this->assertArrayHasKey('CustomFields', $about);
 	}
 
 /**

Plugin/Nodes/Test/Case/Model/NodeTest.php
@@ -226,7 +226,7 @@ class NodeTest extends CroogoTestCase {
 		$filterConditions = $this->Node->filterNodes();
 		$nodes = $this->Node->find('all', array('conditions' => $filterConditions));
 
-		$this->assertEquals(2, count($nodes));
+		$this->assertEquals(3, count($nodes));
 	}
 
 /**
@@ -260,20 +260,20 @@ class NodeTest extends CroogoTestCase {
  * test processActionDelete
  */
 	public function testProcessActionDelete() {
-		$ids = array('1','2');
+		$ids = array('1', '2');
 
 		$success = $this->Node->processAction('delete', $ids);
 		$count = $this->Node->find('count');
 
 		$this->assertTrue($success);
-		$this->assertEquals(0, $count);
+		$this->assertEquals(1, $count);
 	}
 
 /**
  * test processActionPromote
  */
 	public function testProcessActionPromote() {
-		$ids = array('1','2');
+		$ids = array('1', '2');
 
 		$success = $this->Node->processAction('promote', $ids);
 		$newRecords = $this->Node->find('all');
@@ -288,7 +288,7 @@ class NodeTest extends CroogoTestCase {
  * test processActionUnpromote
  */
 	public function testProcessActionUnpromote() {
-		$ids = array('1','2');
+		$ids = array('1', '2', '3');
 
 		$success = $this->Node->processAction('unpromote', $ids);
 		$newRecords = $this->Node->find('all');
@@ -303,7 +303,7 @@ class NodeTest extends CroogoTestCase {
  * test processActionPublish
  */
 	public function testProcessActionPublish() {
-		$ids = array('1','2');
+		$ids = array('1', '2');
 
 		$success = $this->Node->processAction('publish', $ids);
 		$newRecords = $this->Node->find('all');
@@ -318,7 +318,7 @@ class NodeTest extends CroogoTestCase {
  * test processActionUnpublish
  */
 	public function testProcessActionUnpublish() {
-		$ids = array('1','2');
+		$ids = array('1', '2', '3');
 
 		$success = $this->Node->processAction('unpublish', $ids);
 		$newRecords = $this->Node->find('all');

Plugin/Nodes/Test/Fixture/NodeFixture.php
@@ -77,5 +77,28 @@ class NodeFixture extends CroogoTestFixture {
 			'updated' => '2009-12-25 22:00:00',
 			'created' => '2009-12-25 22:00:00'
 		),
+		array(
+			'id' => 3,
+			'parent_id' => null,
+			'user_id' => 1,
+			'title' => 'Protected',
+			'slug' => 'protected',
+			'body' => '<p>This page is only visible to admin</p>',
+			'excerpt' => '',
+			'status' => 1,
+			'mime_type' => '',
+			'comment_status' => 0,
+			'comment_count' => 0,
+			'promote' => 1,
+			'path' => '/page/protected',
+			'terms' => '',
+			'sticky' => 0,
+			'lft' => 3,
+			'rght' => 4,
+			'visibility_roles' => '["1"]',
+			'type' => 'page',
+			'updated' => '2013-09-12 20:00:00',
+			'created' => '2013-09-12 20:00:00'
+		),
 	);
 }