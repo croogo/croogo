239a4ba7c07967dc252aeaea462a649e1598f53a
Rachman Chavik <rchavik@xintesa.com>
2013-09-20 11:55:40 +0700

    Remove 'Log' from CakeLog engine names
    See:
    	http://book.cakephp.org/2.0/en/appendices/2-4-migration-guide.html#logging

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -53,13 +53,14 @@ Configure::write('Dispatcher.filters', array(
 	'AssetDispatcher',
 	'CacheDispatcher'
 ));
+
 CakeLog::config('debug', array(
-	'engine' => 'FileLog',
+	'engine' => 'File',
 	'types' => array('notice', 'info', 'debug'),
 	'file' => 'debug',
 ));
 CakeLog::config('error', array(
-	'engine' => 'FileLog',
+	'engine' => 'File',
 	'types' => array('warning', 'error', 'critical', 'alert', 'emergency'),
 	'file' => 'error',
 ));
