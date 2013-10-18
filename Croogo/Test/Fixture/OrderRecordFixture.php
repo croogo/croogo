dd80ecd92f240a7c5fee77eee8d78732e574f5d4
Damien Biasotto <damien@occi-tech.com>
2013-10-18 21:24:05 +0200

    Add test to underline previous commit

---
+++ Croogo/Test/Fixture/OrderRecordFixture.php
@@ -0,0 +1,27 @@
+<?php
+
+App::uses('CroogoTestFixture', 'Croogo.TestSuite');
+
+class OrderRecordFixture extends CroogoTestFixture {
+
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 20, 'key' => 'primary'),
+		'title' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 60),
+		'weight' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'updated' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_unicode_ci', 'engine' => 'InnoDB')
+	);
+
+	public $records = array(
+		array(
+			'id' => 1,
+			'title' => 'Random record',
+			'weight' => 1,
+			'updated' => '2009-11-02 21:37:38',
+			'created' => '2009-11-02 20:52:00'
+		),
+    );
+
+}
