76aff24d1857f410fd87c4703195fe61a91e5587
Damien Biasotto <damien@occi-tech.com>
2013-10-18 23:29:34 +0200

    Fix CroogoHelperTestSuite Won't run any tests.

--- Test/Case/CroogoHelpersTest.php
+++ Test/Case/CroogoHelpersTest.php
@@ -5,7 +5,7 @@ class CroogoHelpersTest extends PHPUnit_Framework_TestSuite {
 
 	public static function suite() {
 		$suite = new CakeTestSuite('Croogo helper tests');
-		$path = APP . 'Test' . DS . 'Case' . DS . 'View' . DS . 'Helper' . DS;
+		$path =  APP . DS . 'Vendor' . DS . 'croogo' . DS . 'croogo' . DS . 'Croogo' . DS . 'Test' . DS . 'Case' . DS . 'View' . DS . 'Helper' . DS;
 		$suite->addTestDirectory($path);
 		return $suite;
 	}
