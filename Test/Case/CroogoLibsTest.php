d9742122914dfe607fc0f80c968cb84700b987cf
Damien Biasotto <damien@occi-tech.com>
2013-10-18 23:30:59 +0200

    Fix CroogoLibsTest won't run any tests.

--- Test/Case/CroogoLibsTest.php
+++ Test/Case/CroogoLibsTest.php
@@ -5,7 +5,7 @@ class CroogoLibsTest extends PHPUnit_Framework_TestSuite {
 
 	public static function suite() {
 		$suite = new CakeTestSuite('Croogo lib tests');
-		$path = APP . 'Test' . DS . 'Case' . DS . 'Lib' . DS;
+		$path = APP . DS . 'Vendor' . DS . 'croogo' . DS . 'croogo' . DS . 'Croogo' . DS  .'Test' . DS . 'Case' . DS . 'Lib' . DS;
 		$suite->addTestDirectory($path);
 		$suite->addTestDirectory($path . 'Configure');
 		return $suite;
