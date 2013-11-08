543ccc2dd85784a59b092259809cfc42b53d1cb3
Rachman Chavik <rchavik@xintesa.com>
2013-11-08 19:17:57 +0700

    Removing hardcoded references to FileManager

--- Plugin/FileManager/Config/bootstrap.php
+++ Plugin/FileManager/Config/bootstrap.php
@@ -1,5 +1,11 @@
 <?php
 
+Configure::write('Wysiwyg.attachmentBrowseUrl', array(
+	'plugin' => 'file_manager',
+	'controller' => 'attachments',
+	'action' => 'browse',
+));
+
 Croogo::mergeConfig('Wysiwyg.actions', array(
 	'Attachments/admin_browse',
 ));
