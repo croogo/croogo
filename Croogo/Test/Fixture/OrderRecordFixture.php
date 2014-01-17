4ab17d229bf4b277fd56862399630bc277e35de4
Rachman Chavik <rchavik@xintesa.com>
2014-01-17 22:55:27 +0700

    Fix fixture definition

--- Croogo/Test/Fixture/OrderRecordFixture.php
+++ Croogo/Test/Fixture/OrderRecordFixture.php
@@ -9,7 +9,7 @@ class OrderRecordFixture extends CroogoTestFixture {
 		'title' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 60),
 		'weight' => array('type' => 'integer', 'null' => false, 'default' => null),
 		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
-		'updated' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'updated' => array('type' => 'datetime', 'null' => false, 'default' => null),
 		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
 		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_unicode_ci', 'engine' => 'InnoDB')
 	);
