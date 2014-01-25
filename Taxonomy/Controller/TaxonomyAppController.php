f3b39774a285f302b02ff4cce57de235e8e90b10
Damien Biasotto <damien@occitech.fr>
2014-01-25 20:47:48 +0100

    Fix bad namespace and replace use of cake app controller for croogo one.

--- Taxonomy/Controller/TaxonomyAppController.php
+++ Taxonomy/Controller/TaxonomyAppController.php
@@ -1,4 +1,5 @@
 <?php
+App::uses('CroogoAppController', 'Croogo.Controller');
 /**
  * Taxonomy App Controller
  *
@@ -9,6 +10,6 @@
  * @license  http://www.opensource.org/licenses/mit-license.php The MIT License
  * @link     http://www.croogo.org
  */
-class TaxonomyAppController extends AppController {
+class TaxonomyAppController extends CroogoAppController {
 
 }
