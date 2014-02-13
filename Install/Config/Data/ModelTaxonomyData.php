576c913776b00e167acee867852e9d25aa9e007c
Rachman Chavik <rchavik@xintesa.com>
2014-02-13 19:07:09 +0700

    Fix: Rename datafile for ModelTaxonomy

---
+++ Install/Config/Data/ModelTaxonomyData.php
@@ -0,0 +1,15 @@
+<?php
+class ModelTaxonomyData {
+
+	public $table = 'model_taxonomies';
+
+	public $records = array(
+		array(
+			'id' => '1',
+			'model' => 'Node',
+			'foreign_key' => '1',
+			'taxonomy_id' => '1'
+		),
+	);
+
+}
