9249c0952178d65a92203eec70a8a46ceb5462c0
Appsbender <regz24@gmail.com>
2014-02-20 17:15:21 -0800

    Fix accidental assignment instead of comparing variable

--- Plugin/Croogo/Config/croogo_bootstrap.php
+++ Plugin/Croogo/Config/croogo_bootstrap.php
@@ -52,7 +52,7 @@ App::build(array(
 	'View' => array($croogoPath . 'View' . DS),
 	'View/Helper' => array($croogoPath . 'View' . DS . 'Helper' . DS),
 ), App::APPEND);
-if ($theme = Configure::read('Site.theme')) {
+if ($theme == Configure::read('Site.theme')) {
 	App::build(array(
 		'View/Helper' => array(App::themePath($theme) . 'Helper' . DS),
 	));
