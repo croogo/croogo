d0d3f5f2f2b3db0dda85ed3ab82d4bf7ac31f74f
Appsbender <regz24@gmail.com>
2014-03-15 10:26:19 -0700

    Fix Accidental Assignment variable $adminTheme

--- Croogo/Controller/CroogoErrorController.php
+++ Croogo/Controller/CroogoErrorController.php
@@ -71,7 +71,7 @@ class CroogoErrorController extends AppController {
 		if (Configure::read('Site.theme') && !isset($this->request->params['admin'])) {
 			$this->theme = Configure::read('Site.theme');
 		} elseif (isset($this->request->params['admin'])) {
-			if ($adminTheme = Configure::read('Site.admin_theme')) {
+			if ($adminTheme == Configure::read('Site.admin_theme')) {
 				$this->theme = $adminTheme;
 			}
 			$this->layout = 'admin_full';
