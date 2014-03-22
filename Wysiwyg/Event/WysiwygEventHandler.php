9771b599f2dc1a14d8d2e6fde29d9d465d16d772
Rachman Chavik <rchavik@xintesa.com>
2014-03-22 15:56:41 +0700

    Add forgotten file Event/WysiwygEventHandler.php *facepalm*

---
+++ Wysiwyg/Event/WysiwygEventHandler.php
@@ -0,0 +1,32 @@
+<?php
+
+App::uses('CakeEventListener', 'Event');
+
+/**
+ * Wysiwyg Event Handler
+ *
+ * @category Event
+ * @package  Croogo.Ckeditor
+ * @license  http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @link     http://www.croogo.org
+ */
+class WysiwygEventHandler implements CakeEventListener {
+
+/**
+ * implementedEvents
+ *
+ * @return array
+ */
+	public function implementedEvents() {
+		return array(
+			'Croogo.bootstrapComplete' => array(
+				'callable' => 'onBootstrapComplete',
+			),
+		);
+	}
+
+	public function onBootstrapComplete($event) {
+		Croogo::hookHelper('*', 'Wysiwyg.Wysiwyg');
+	}
+
+}
