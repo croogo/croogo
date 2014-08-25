c6d092c7d723dd8d69dc72c22a903593c55ab70b
Walther Lalk <walther@octoplus.co.za>
2014-08-25 12:58:07 +0200

    Add migration for enable wysiwyg field

--- Taxonomy/Config/Schema/schema.php
+++ Taxonomy/Config/Schema/schema.php
@@ -1,5 +1,4 @@
-<?php
-
+<?php 
 class TaxonomySchema extends CakeSchema {
 
 	public function before($event = array()) {
@@ -9,13 +8,24 @@ class TaxonomySchema extends CakeSchema {
 	public function after($event = array()) {
 	}
 
+	public $model_taxonomies = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 20, 'unsigned' => false, 'key' => 'primary'),
+		'model' => array('type' => 'string', 'null' => false, 'default' => 'Node', 'length' => 50, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'foreign_key' => array('type' => 'integer', 'null' => false, 'default' => '0', 'length' => 20, 'unsigned' => false),
+		'taxonomy_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 20, 'unsigned' => false),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_unicode_ci', 'engine' => 'InnoDB')
+	);
+
 	public $taxonomies = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 20, 'key' => 'primary'),
-		'parent_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 20),
-		'term_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10),
-		'vocabulary_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10),
-		'lft' => array('type' => 'integer', 'null' => true, 'default' => null),
-		'rght' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 20, 'unsigned' => false, 'key' => 'primary'),
+		'parent_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 20, 'unsigned' => false),
+		'term_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => false),
+		'vocabulary_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => false),
+		'lft' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
+		'rght' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1)
 		),
@@ -23,12 +33,14 @@ class TaxonomySchema extends CakeSchema {
 	);
 
 	public $terms = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'key' => 'primary'),
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => false, 'key' => 'primary'),
 		'title' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
 		'slug' => array('type' => 'string', 'null' => false, 'default' => null, 'key' => 'unique', 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
 		'description' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
 		'updated' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'updated_by' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 20, 'unsigned' => false),
 		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'created_by' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 20, 'unsigned' => false),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1),
 			'slug' => array('column' => 'slug', 'unique' => 1)
@@ -37,20 +49,23 @@ class TaxonomySchema extends CakeSchema {
 	);
 
 	public $types = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'key' => 'primary'),
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => false, 'key' => 'primary'),
 		'title' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
 		'alias' => array('type' => 'string', 'null' => false, 'default' => null, 'key' => 'unique', 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
 		'description' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
 		'format_show_author' => array('type' => 'boolean', 'null' => false, 'default' => '1'),
 		'format_show_date' => array('type' => 'boolean', 'null' => false, 'default' => '1'),
-		'comment_status' => array('type' => 'integer', 'null' => false, 'default' => '1', 'length' => 1),
+		'format_use_wysiwyg' => array('type' => 'boolean', 'null' => false, 'default' => '1'),
+		'comment_status' => array('type' => 'integer', 'null' => false, 'default' => '1', 'length' => 1, 'unsigned' => false),
 		'comment_approve' => array('type' => 'boolean', 'null' => false, 'default' => '1'),
 		'comment_spam_protection' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
 		'comment_captcha' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
 		'params' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
 		'plugin' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
 		'updated' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'updated_by' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 20, 'unsigned' => false),
 		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'created_by' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 20, 'unsigned' => false),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1),
 			'type_alias' => array('column' => 'alias', 'unique' => 1)
@@ -59,10 +74,10 @@ class TaxonomySchema extends CakeSchema {
 	);
 
 	public $types_vocabularies = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'key' => 'primary'),
-		'type_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10),
-		'vocabulary_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10),
-		'weight' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => false, 'key' => 'primary'),
+		'type_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => false),
+		'vocabulary_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => false),
+		'weight' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1)
 		),
@@ -70,7 +85,7 @@ class TaxonomySchema extends CakeSchema {
 	);
 
 	public $vocabularies = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'key' => 'primary'),
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => false, 'key' => 'primary'),
 		'title' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
 		'alias' => array('type' => 'string', 'null' => false, 'default' => null, 'key' => 'unique', 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
 		'description' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
@@ -78,9 +93,11 @@ class TaxonomySchema extends CakeSchema {
 		'multiple' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
 		'tags' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
 		'plugin' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
-		'weight' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'weight' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
 		'updated' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'updated_by' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 20, 'unsigned' => false),
 		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'created_by' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 20, 'unsigned' => false),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1),
 			'vocabulary_alias' => array('column' => 'alias', 'unique' => 1)
