f4944087c11081c8a6ed68cbf48d71d44249d47e
Damien Biasotto <damien.biasotto@gmail.com>
2014-09-12 22:03:05 +0200

    Created rename method and covered it with tests.

--- FileManager/Model/FileManager.php
+++ FileManager/Model/FileManager.php
@@ -1,7 +1,7 @@
 <?php
 
 App::uses('FileManagerAppModel', 'FileManager.Model');
-
+App::uses('File', 'Utility');
 /**
  * FileManager Model
  *
@@ -27,6 +27,15 @@ class FileManager extends FileManagerAppModel {
 		}
 
 		return false;
+	}
+
+	public function rename($oldPath, $newPath) {
+		if (is_dir($oldPath)) {
+			$Folder = new Folder($oldPath);
+			return $Folder->move(array('from' => $oldPath, 'to' => $newPath));
+		} else {
+			return rename($oldPath, $newPath);
+		}
 	}
 
 	private function __isWithinPath($referencePath, $pathToCheck) {
