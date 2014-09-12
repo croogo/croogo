c294e10401eba2ee88da5badaa1896f1586e4a2b
Damien Biasotto <damien.biasotto@gmail.com>
2014-09-12 22:03:05 +0200

    Create rename() method and cover it with tests

--- FileManager/Model/FileManager.php
+++ FileManager/Model/FileManager.php
@@ -1,7 +1,7 @@
 <?php
 
 App::uses('FileManagerAppModel', 'FileManager.Model');
-
+App::uses('File', 'Utility');
 /**
  * FileManager Model
  *
@@ -60,6 +60,22 @@ class FileManager extends FileManagerAppModel {
 		}
 
 		return false;
+	}
+
+/**
+ * Rename $oldPath to $newPath
+ *
+ * @param string $oldPath Old filename/directory
+ * @param string $newPath New filename/directory
+ * @return bool True if rename was successful
+ */
+	public function rename($oldPath, $newPath) {
+		if (is_dir($oldPath)) {
+			$Folder = new Folder($oldPath);
+			return $Folder->move(array('from' => $oldPath, 'to' => $newPath));
+		} else {
+			return rename($oldPath, $newPath);
+		}
 	}
 
 /**
--- FileManager/Test/Case/Model/FileManagerTest.php
+++ FileManager/Test/Case/Model/FileManagerTest.php
@@ -16,52 +16,74 @@ class FileManagerTest extends CroogoTestCase {
 		$this->FileManager = new FileManager(false, null, null, null);
 		$this->__testAppPath = CakePlugin::path('FileManager') . 'Test' . DS . 'test_app' . DS;
 		$this->__setFilePathsForTests();
-		$this->__setupTestFileSystemStructure();
 		parent::setUp();
 	}
 
 	public function tearDown() {
 		unset($this->FileManager);
-		$this->__tearDownTestFileSystemStructure();
 		parent::tearDown();
 	}
 
+/**
+ * @group isEditable
+ */
 	public function testIsEditableShouldReturnTrueWhenPathIsWithinEditablePaths() {
 		$isEditable = $this->FileManager->isEditable($this->__testAppPath . DS . 'renameMeTooPlease.txt');
 		$this->assertTrue($isEditable);
 	}
 
+/**
+ * @group isEditable
+ */
 	public function testIsEditableShouldReturnFalseWhenPathIsOutsideEditablePaths() {
 		$isEditable = $this->FileManager->isEditable('/var/log/apache2');
 		$this->assertFalse($isEditable);
 	}
 
 
-	/**
-	 * Convenient methods for testsuite
-	 */
+/**
+ * @group rename
+ */
+	public function testRenameShouldReturnedTrueOnSuccess() {
+		$oldPath = $this->__testAppPath . DS . 'renameMe';
+		$newPath = $this->__testAppPath . DS . 'renamed';
+
+		$this->assertTrue($this->FileManager->rename($oldPath, $newPath));
+		$this->FileManager->rename($newPath, $oldPath);
+	}
+
+/**
+ * @group rename
+ */
+	public function testRenameShouldRenamedOldFileToNewFile() {
+		$oldPath = $this->__testAppPath . DS . 'renameMeTooPlease.txt';
+		$newPath = $this->__testAppPath . DS . 'renamed.txt';
+
+		$this->FileManager->rename($oldPath, $newPath);
+		$this->assertTrue(file_exists($newPath) && !file_exists($oldPath));
+
+		$this->FileManager->rename($newPath, $oldPath);
+	}
+
+/**
+ * @group rename
+ */
+	public function testRenameShouldRenamedOldFolderToNewFolder() {
+		$oldPath = $this->__testAppPath . 'renameMe';
+		$newPath = $this->__testAppPath . 'renamed';
+
+		$this->FileManager->rename($oldPath, $newPath);
+		$this->assertTrue(is_dir($newPath) && !is_dir($oldPath));
+
+		$this->FileManager->rename($newPath, $oldPath);
+	}
+
+/**
+ * Convenient methods for testsuite
+ */
 	private function __setFilePathsForTests() {
 		Configure::write('FileManager.editablePaths', array($this->__testAppPath));
 		Configure::write('FileManager.deletablePaths', array($this->__testAppPath));
-	}
-
-	private function __setupTestFileSystemStructure() {
-		mkdir($this->__testAppPath . DS . 'deleteMe');
-		mkdir($this->__testAppPath . DS . 'renameMe');
-		touch($this->__testAppPath . DS . 'deleteMe' . DS . 'toTestRecursiveDeletion.txt');
-		touch($this->__testAppPath . DS . 'renameMe' . DS . 'toBeRenamedSubFile.txt');
-		touch($this->__testAppPath . DS . 'deleteMeTooPlease.txt');
-		touch($this->__testAppPath . DS . 'renameMeTooPlease.txt');
-
-	}
-
-	private function __tearDownTestFileSystemStructure() {
-		@unlink($this->__testAppPath . DS . 'deleteMe' . DS . 'toTestRecursiveDeletion.txt');
-		@unlink($this->__testAppPath . DS . 'renameMe' . DS . 'toBeRenamedSubFile.txt');
-		@unlink($this->__testAppPath . DS . 'deleteMeTooPlease.txt');
-		@unlink($this->__testAppPath . DS . 'renameMeTooPlease.txt');
-		rmdir($this->__testAppPath . DS . 'deleteMe');
-		rmdir($this->__testAppPath . DS . 'renameMe');
 	}
 
 }
