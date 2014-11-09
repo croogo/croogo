croogo/croogo

commit 0b4df30693f6ffbce830bc648bd1df4e89217058

Fix bad namespace and replace use of cake app controller for croogo one.

Taxonomy/Controller/TaxonomyAppController.php
@@ -1,4 +1,5 @@
 <?php
+App::uses('CroogoAppController', 'Croogo.Controller');
 /**
  * Taxonomy App Controller
  *
@@ -9,6 +10,6 @@
  * @license  http://www.opensource.org/licenses/mit-license.php The MIT License
  * @link     http://www.croogo.org
  */
-class TaxonomyAppController extends AppController {
+class TaxonomyAppController extends CroogoAppController {
 
 }

commit d0c45f7a20a24a958c0a2ef70991e26852abd5f5

Fix bad inheritance for TaxonomyAppController. refs #518
Regression introduced in commit 0b4df30.

Taxonomy/Controller/TaxonomyAppController.php
@@ -1,5 +1,5 @@
 <?php
-App::uses('CroogoAppController', 'Croogo.Controller');
+App::uses('AppController', 'Controller');
 /**
  * Taxonomy App Controller
  *
@@ -10,6 +10,6 @@ App::uses('CroogoAppController', 'Croogo.Controller');
  * @license  http://www.opensource.org/licenses/mit-license.php The MIT License
  * @link     http://www.croogo.org
  */
-class TaxonomyAppController extends CroogoAppController {
+class TaxonomyAppController extends AppController {
 
 }