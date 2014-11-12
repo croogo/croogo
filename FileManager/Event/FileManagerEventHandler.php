9e270df1b54570ec3a432a734fe9907dd97ceb8b
Mike Tallroth <mike.tallroth@gmail.com>
2014-11-12 09:12:25 -0600

    Adds attachment choosers (separate Image and File choosers) on the setupLinkChoosers event.

--- FileManager/Event/FileManagerEventHandler.php
+++ FileManager/Event/FileManagerEventHandler.php
@@ -5,6 +5,7 @@ App::uses('CakeEventListener', 'Event');
 /**
  * FileManagerEventHandler
  *
+ * @category Event
  * @package  Croogo.FileManager.Event
  * @license  http://www.opensource.org/licenses/mit-license.php The MIT License
  * @link     http://www.croogo.org
@@ -15,7 +16,55 @@ class FileManagerEventHandler implements CakeEventListener {
  * implementedEvents
  */
 	public function implementedEvents() {
-		return array();
+		return array(
+			'Controller.Links.setupLinkChooser' => array(
+				'callable' => 'onSetupLinkChooser',
+			),
+		);
+	}
+
+/**
+ * Setup Link chooser values
+ *
+ * @return void
+ */
+	public function onSetupLinkChooser($event) {
+		$linkChoosers = array();
+		$linkChoosers['Images'] = array(
+			'title' => 'Images',
+			'description' => 'Attachments with an image mime type.',
+			'url' => array(
+				'plugin' => 'file_manager',
+				'controller' => 'attachments',
+				'action' => 'index',
+				'?' => array(
+					'chooser_type' => 'image',
+					'chooser' => 1,
+					'KeepThis' => true,
+					'TB_iframe' => true,
+					'height' => 400,
+					'width' => 600
+				)
+			)
+		);
+		$linkChoosers['Files'] = array(
+			'title' => 'Files',
+			'description' => 'Attachments with other mime types, ie. pdf, xls, doc, etc.',
+			'url' => array(
+				'plugin' => 'file_manager',
+				'controller' => 'attachments',
+				'action' => 'index',
+				'?' => array(
+					'chooser_type' => 'file',
+					'chooser' => 1,
+					'KeepThis' => true,
+					'TB_iframe' => true,
+					'height' => 400,
+					'width' => 600
+				)
+			)
+		);
+		Croogo::mergeConfig('Menus.linkChoosers', $linkChoosers);
 	}
 
 }
