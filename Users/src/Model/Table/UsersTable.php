d8cd7f1ca4996ddacf4d3c57964db7dcd5643dfc
Wouter van Os <wouter@wouter0100.nl>
2015-04-23 01:49:07 +0200

    Added validation to the user

--- Users/src/Model/Table/UsersTable.php
+++ Users/src/Model/Table/UsersTable.php
@@ -2,6 +2,7 @@
 
 namespace Croogo\Users\Model\Table;
 
+use Cake\Validation\Validator;
 use Croogo\Croogo\Model\Table\CroogoTable;
 
 class UsersTable extends CroogoTable {
@@ -35,4 +36,77 @@ class UsersTable extends CroogoTable {
 		$this->belongsTo('Croogo/Users.Roles');
 		$this->addBehavior('Search.Searchable');
 	}
+
+	public function validationDefault(Validator $validator) {
+		return $validator
+			->add('username', [
+				'notEmpty' => [
+					'rule' => 'notEmpty',
+					'message' => 'The username has already been taken.',
+					'last' => true
+				],
+				'validateUnique' => [
+					'rule' => 'validateUnique',
+					'provider' => 'table',
+					'message' => 'The username has already been taken.',
+					'last' => true
+				],
+				'alphaNumeric' => [
+					'rule' => 'alphaNumeric',
+					'message' => 'This field must be alphanumeric',
+					'last' => true
+				]
+			])
+			->add('email', [
+				'notEmpty' => [
+					'rule' => 'notEmpty',
+					'message' => 'The username has already been taken.',
+					'last' => true
+				],
+				'email' => [
+					'rule' => 'email',
+					'message' => 'Please provide a valid email address.',
+					'last' => true
+				],
+				'validateUnique' => [
+					'rule' => 'validateUnique',
+					'provider' => 'table',
+					'message' => 'Email address already in use.',
+					'last' => true
+				]
+			])
+			->add('password', [
+				'minLength' => [
+					'rule' => ['minLength', 6],
+					'message' => 'Passwords must be at least 6 characters long.',
+					'last' => true
+				],
+				'compareWith' => [
+					'rule' => ['compareWith', 'verify_password'],
+					'message' => 'Passwords do not match. Please, try again',
+					'last' => true
+				]
+			])
+			->add('name', [
+				'notEmpty' => [
+					'rule' => 'notEmpty',
+					'message' => 'This field cannot be left blank.',
+					'last' => true
+				],
+//				'alphaNumeric' => [
+//					'rule' => 'alphaNumeric',
+//					'message' => 'This field must be alphanumeric',
+//					'last' => true
+//				]
+			])
+			->allowEmpty('website')
+			->add('website', [
+				'url' => [
+					'rule' => 'url',
+					'message' => 'This field must be a valid URL',
+					'last' => true
+				]
+			]);
+	}
+
 }
