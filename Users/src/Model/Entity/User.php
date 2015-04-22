d1c64e488b0c4e6d3832cfde46d541e24085c388
Wouter van Os <wouter@wouter0100.nl>
2015-04-23 01:08:24 +0200

    Added Hasher to the User

--- Users/src/Model/Entity/User.php
+++ Users/src/Model/Entity/User.php
@@ -2,8 +2,12 @@
 
 namespace Croogo\Users\Model\Entity;
 
+use Cake\Auth\DefaultPasswordHasher;
 use Cake\ORM\Entity;
 
 class User extends Entity {
 
+    protected function _setPassword($password) {
+        return (new DefaultPasswordHasher)->hash($password);
+    }
 }
