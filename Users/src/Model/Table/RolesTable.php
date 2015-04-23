28156c85560f4830944dae5cb6e0c557e981cdf5
Wouter van Os <wouter@wouter0100.nl>
2015-04-23 13:19:23 +0200

    Added Display Fields to the Roles table for the index

--- Users/src/Model/Table/RolesTable.php
+++ Users/src/Model/Table/RolesTable.php
@@ -6,4 +6,14 @@ use Croogo\Croogo\Model\Table\CroogoTable;
 
 class RolesTable extends CroogoTable {
 
+/**
+ * Display fields for this model
+ *
+ * @var array
+ */
+    protected $_displayFields = array(
+        'id',
+        'title',
+        'alias',
+    );
 }
