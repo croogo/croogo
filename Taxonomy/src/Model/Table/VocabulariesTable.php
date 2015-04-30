9bb8c51e2fdac57a7d1f2cf7554eb5a4ba4fb366
Marlin Cremers <m.cremers@cvo-technologies.com>
2015-04-30 16:30:03 +0200

    Setup Taxonomies relation in Vocabularies

--- Taxonomy/src/Model/Table/VocabulariesTable.php
+++ Taxonomy/src/Model/Table/VocabulariesTable.php
@@ -4,6 +4,9 @@ namespace Croogo\Taxonomy\Model\Table;
 
 use Croogo\Croogo\Model\Table\CroogoTable;
 
+/**
+ * @property TaxonomiesTable Taxonomies
+ */
 class VocabulariesTable extends CroogoTable {
 
 	public function initialize(array $config) {
@@ -12,6 +15,9 @@ class VocabulariesTable extends CroogoTable {
 		]);
 		$this->belongsToMany('Croogo/Taxonomy.Types', [
 			'joinTable' => 'types_vocabularies',
+		]);
+		$this->hasMany('Croogo/Taxonomy.Taxonomies', [
+			'dependent' => true,
 		]);
 	}
 
