8720e00c4f3a242c71b105a1399f495e19ba71b5
Marlin Cremers <m.cremers@cvo-technologies.com>
2015-05-23 22:12:52 +0200

    Get parts of the publishable behavior to work

--- Croogo/src/Model/Behavior/PublishableBehavior.php
+++ Croogo/src/Model/Behavior/PublishableBehavior.php
@@ -2,8 +2,9 @@
 
 namespace Croogo\Croogo\Model\Behavior;
 
-use App\Controller\Component\AuthComponent;
-use Croogo\Lib\CroogoStatus;
+use Cake\ORM\Behavior;
+use Croogo\Croogo\CroogoStatus;
+
 /**
  * Publishable Behavior
  *
@@ -19,35 +20,39 @@ use Croogo\Lib\CroogoStatus;
  * @link     http://www.croogo.org
  * @see      CroogoStatus
  */
-class PublishableBehavior extends ModelBehavior {
+class PublishableBehavior extends Behavior {
+
+	protected $_defaultConfig = [
+		'admin' => false,
+		'fields' => [
+			'publish_start' => 'publish_start',
+			'publish_end' => 'publish_end',
+		],
+	];
 
 /**
- * Setup
- *
- * Valid options:
- *
- *   `admin`: Enable/disable date filtering for users with Admin roles
- *   `fields`: Specifies the physical field name to use
+* Setup
+*
+* Valid options:
+*
+*   `admin`: Enable/disable date filtering for users with Admin roles
+*   `fields`: Specifies the physical field name to use
  *
  * @return void
  */
-	public function setup(Model $model, $config = array()) {
-		$this->settings[$model->alias] = Hash::merge(array(
-			'admin' => false,
-			'fields' => array(
-				'publish_start' => 'publish_start',
-				'publish_end' => 'publish_end',
-			),
-		), $config);
+	public function initialize(array $config) {
+		parent::initialize($config);
+
 		$this->_CroogoStatus = new CroogoStatus();
 	}
 
-/**
+
+	/**
  * Get status for conditions in query based on current user's role id
  *
  * @return array Array of status
  */
-	public function status(Model $model, $statusType = 'publishing', $accessType = 'public') {
+	public function status($statusType = 'publishing', $accessType = 'public') {
 		return $this->_CroogoStatus->status($statusType, $accessType);
 	}
 
@@ -56,53 +61,53 @@ class PublishableBehavior extends ModelBehavior {
  *
  * @return array Options passed to Model::find()
  */
-	public function beforeFind(Model $model, $query = array()) {
-		$settings = $this->settings[$model->alias];
-		if (!$model->Behaviors->enabled('Publishable')) {
-			return $query;
-		}
-
-		if ($settings['admin'] === false) {
-			if (AuthComponent::user('role_id') == 1) {
-				return $query;
-			}
-		}
-
-		if (!$model->hasField($settings['fields']['publish_start']) ||
-			!$model->hasField($settings['fields']['publish_end'])
-		) {
-			return $query;
-		}
-
-		$date = isset($query['date']) ? $query['date'] : date('Y-m-d H:i:s');
-		$start = $model->escapeField($settings['fields']['publish_start']);
-		$end = $model->escapeField($settings['fields']['publish_end']);
-
-		if (is_string($query['conditions'])) {
-			$query['conditions'] = (array)$query['conditions'];
-		}
-
-		$query['conditions'][] = array(
-			'OR' => array(
-				$start => null,
-				array(
-					$start . ' <> ' => null,
-					$start . ' <=' => $date,
-				),
-			),
-		);
-
-		$query['conditions'][] = array(
-			'OR' => array(
-				$end => null,
-				array(
-					$end . ' <> ' => null,
-					$end . ' >=' => $date,
-				),
-			),
-		);
-
-		return $query;
-	}
+//	public function beforeFind(Model $model, $query = array()) {
+//		$settings = $this->settings[$model->alias];
+//		if (!$model->Behaviors->enabled('Publishable')) {
+//			return $query;
+//		}
+//
+//		if ($settings['admin'] === false) {
+//			if (AuthComponent::user('role_id') == 1) {
+//				return $query;
+//			}
+//		}
+//
+//		if (!$model->hasField($settings['fields']['publish_start']) ||
+//			!$model->hasField($settings['fields']['publish_end'])
+//		) {
+//			return $query;
+//		}
+//
+//		$date = isset($query['date']) ? $query['date'] : date('Y-m-d H:i:s');
+//		$start = $model->escapeField($settings['fields']['publish_start']);
+//		$end = $model->escapeField($settings['fields']['publish_end']);
+//
+//		if (is_string($query['conditions'])) {
+//			$query['conditions'] = (array)$query['conditions'];
+//		}
+//
+//		$query['conditions'][] = array(
+//			'OR' => array(
+//				$start => null,
+//				array(
+//					$start . ' <> ' => null,
+//					$start . ' <=' => $date,
+//				),
+//			),
+//		);
+//
+//		$query['conditions'][] = array(
+//			'OR' => array(
+//				$end => null,
+//				array(
+//					$end . ' <> ' => null,
+//					$end . ' >=' => $date,
+//				),
+//			),
+//		);
+//
+//		return $query;
+//	}
 
 }
