783a02ba5daa4587dc6713b4a4baa113d67afdf8
Marlin Cremers <m.cremers@cvo-technologies.com>
2015-05-24 22:49:18 +0200

    Add routes file for Menus

---
+++ Menus/config/routes.php
@@ -0,0 +1,13 @@
+<?php
+
+use Croogo\Croogo\CroogoRouter;
+
+CroogoRouter::connect('/admin/menus/menus/:action/*', array(
+	'prefix' => 'admin',
+	'plugin' => 'Croogo/Menus', 'controller' => 'Menus',
+));
+
+CroogoRouter::connect('/admin/menus/links/:action/*', array(
+	'prefix' => 'admin',
+	'plugin' => 'Croogo/Menus', 'controller' => 'Links',
+));
