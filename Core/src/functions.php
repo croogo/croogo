e60767f83bcd553ad42fd71e062562a8b27e8a9b
Marlin Cremers <m.cremers@cvo-technologies.com>
2015-08-11 21:19:35 +0200

    Add functions to call the link class

---
+++ Core/src/functions.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Croogo\Core;
+
+use Croogo\Core\Link;
+
+if (!function_exists('\Croogo\Core\linkFromLinkString')) {
+	/**
+	 * @param string $link
+	 *
+	 * @return \Croogo\Core\Link
+	 */
+	function linkFromLinkString($link) {
+		return Link::createFromLinkString($link);
+	}
+}
+
+if (!function_exists('\Croogo\Core\link')) {
+	/**
+	 * @param array|string $url
+	 *
+	 * @return \Croogo\Core\Link
+	 */
+	function link($url) {
+		return new Link($url);
+	}
+}
