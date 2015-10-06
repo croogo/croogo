f75b1da24c907642191b1de5783d1abe66f58a9f
Wouter van Os <wouter@wouter0100.nl>
2015-10-06 21:51:33 +0200

    Removed options from Auth in CroogoAppController

--- Core/src/Controller/CroogoAppController.php
+++ Core/src/Controller/CroogoAppController.php
@@ -42,17 +42,7 @@ class CroogoAppController extends AppController {
 		'Croogo/Acl.Filter',
 		'Security',
 		'Acl.Acl',
-		'Auth' => [
-			'authenticate' => [
-				'Form' => [
-					'passwordHasher' => [
-						'className' => 'Fallback',
-						'hashers' => ['Default', 'Weak']
-					],
-					'userModel' => 'Croogo/Users.Users'
-				]
-			]
-		],
+		'Auth',
 		'Flash',
 		'RequestHandler',
 	);
