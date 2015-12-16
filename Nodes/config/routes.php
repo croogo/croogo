5c6f0b64f62eb1f41904115c6e3e6e6985f2c4e6
Wouter van Os <wouter@wouter0100.nl>
2015-12-16 22:51:28 +0100

    Updated Nodes routes to the new style
    See issue #693 for the discussion.

--- Nodes/config/routes.php
+++ Nodes/config/routes.php
@@ -1,62 +1,34 @@
 <?php
 
-use Cake\Core\Configure;
 use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
-use Croogo\Core\CroogoRouter;
 
-Router::plugin('Croogo/Nodes', function (RouteBuilder $routeBuilder) {
-    $routeBuilder->fallbacks();
+Router::plugin('Croogo/Nodes', ['path' => '/'], function (RouteBuilder $routeBuilder) {
+    $routeBuilder->prefix('admin', function (RouteBuilder $routeBuilder) {
+        $routeBuilder->extensions(['json']);
+
+        $routeBuilder->connect('/nodes/:action/*', ['controller' => 'Nodes']);
+    });
+
+    $routeBuilder->connect('/', ['controller' => 'Nodes', 'action' => 'promoted']);
+    $routeBuilder->connect('/promoted/*', ['controller' => 'Nodes', 'action' => 'promoted']);
+    $routeBuilder->connect('/search/*', ['controller' => 'Nodes', 'action' => 'search']);
+
+    // Content types
+    $routeBuilder->connect('/blog', ['controller' => 'Nodes', 'action' => 'index', 'type' => 'blog']);
+    $routeBuilder->connect('/blog/archives/*', ['controller' => 'Nodes', 'action' => 'index', 'type' => 'blog']);
+    $routeBuilder->connect('/blog/:slug', ['controller' => 'Nodes', 'action' => 'view', 'type' => 'blog']);
+    $routeBuilder->connect('/blog/term/:slug/*', ['controller' => 'Nodes', 'action' => 'term', 'type' => 'blog']);
+
+    $routeBuilder->connect('/node', ['controller' => 'Nodes', 'action' => 'index', 'type' => 'node']);
+    $routeBuilder->connect('/node/archives/*', ['controller' => 'Nodes', 'action' => 'index', 'type' => 'node']);
+    $routeBuilder->connect('/node/:slug', ['controller' => 'Nodes', 'action' => 'view', 'type' => 'node']);
+    $routeBuilder->connect('/node/term/:slug/*', ['controller' => 'Nodes', 'action' => 'term', 'type' => 'node']);
+
+    // Page
+    $routeBuilder->connect('/about', ['controller' => 'Nodes', 'action' => 'view', 'type' => 'page', 'slug' => 'about']);
+    $routeBuilder->connect('/page/:slug', ['controller' => 'Nodes', 'action' => 'view', 'type' => 'page']);
+
+    // Node view
+    $routeBuilder->connect('/nodes/view/:type/:slug', ['controller' => 'Nodes', 'action' => 'view']);
 });
-
-CroogoRouter::mapResources('Nodes.Nodes', [
-    'prefix' => '/:api/:prefix/',
-]);
-
-Router::connect('/:api/:prefix/nodes/lookup', [
-    'plugin' => 'Croogo/Nodes',
-    'controller' => 'Nodes',
-    'action' => 'lookup',
-], [
-    'routeClass' => 'Croogo\Core\Routing\Route\ApiRoute',
-]);
-
-// Basic
-CroogoRouter::connect('/', [
-    'plugin' => 'Croogo/Nodes', 'controller' => 'Nodes', 'action' => 'promoted'
-]);
-
-CroogoRouter::connect('/promoted/*', [
-    'plugin' => 'Croogo/Nodes', 'controller' => 'Nodes', 'action' => 'promoted'
-]);
-
-CroogoRouter::connect('/search/*', [
-    'plugin' => 'Croogo/Nodes', 'controller' => 'Nodes', 'action' => 'search'
-]);
-
-// Content types
-CroogoRouter::contentType('blog');
-CroogoRouter::contentType('node');
-if (Configure::read('Croogo.installed')) {
-    CroogoRouter::routableContentTypes();
-}
-
-// Page
-CroogoRouter::connect('/about', [
-    'plugin' => 'Croogo/Nodes', 'controller' => 'Nodes', 'action' => 'view',
-    'type' => 'page', 'slug' => 'about'
-]);
-CroogoRouter::connect('/page/:slug', [
-    'plugin' => 'Croogo/Nodes', 'controller' => 'Nodes', 'action' => 'view',
-    'type' => 'page'
-]);
-
-CroogoRouter::connect('/admin/nodes/:action/*', [
-    'prefix' => 'admin',
-    'plugin' => 'Croogo/Nodes', 'controller' => 'Nodes',
-]);
-
-CroogoRouter::connect('/nodes/view/:type/:slug', [
-    'plugin' => 'Croogo/Nodes', 'controller' => 'Nodes',
-    'action' => 'view'
-]);
