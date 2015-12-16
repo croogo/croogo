d8dbe5b230c4da8201e67783c143f5a9e45be342
Wouter van Os <wouter@wouter0100.nl>
2015-12-16 22:53:16 +0100

    Updated Users routes to the new style
    See issue #693 for the discussion.

--- Users/config/routes.php
+++ Users/config/routes.php
@@ -2,36 +2,24 @@
 
 use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
-use Croogo\Core\CroogoRouter;
 
-Router::plugin('Croogo/Users', function (RouteBuilder $routeBuilder) {
-    $routeBuilder->fallbacks();
+Router::plugin('Croogo/Users', ['path' => '/'], function (RouteBuilder $routeBuilder) {
+    $routeBuilder->prefix('admin', function (RouteBuilder $routeBuilder) {
+        $routeBuilder->extensions(['json']);
+
+        $routeBuilder->connect('/users', ['controller' => 'Users', 'action' => 'index']);
+        $routeBuilder->connect('/users/:action/*', ['controller' => 'Users']);
+
+        $routeBuilder->connect('/roles', ['controller' => 'Roles', 'action' => 'index']);
+        $routeBuilder->connect('/roles/:action/*', ['controller' => 'Roles']);
+    });
+
+    $routeBuilder->connect('/register', ['controller' => 'Users', 'action' => 'add']);
+    $routeBuilder->connect('/user/:username', ['controller' => 'Users', 'action' => 'view'], ['pass' => ['username']]);
+
+    $routeBuilder->connect('/users', ['controller' => 'Users', 'action' => 'index']);
+    $routeBuilder->connect('/users/:action/*', ['controller' => 'Users']);
+
+    $routeBuilder->connect('/roles', ['controller' => 'Roles', 'action' => 'index']);
+    $routeBuilder->connect('/roles/:action/*', ['controller' => 'Roles']);
 });
-
-CroogoRouter::mapResources('Users.Users', [
-    'prefix' => '/:api/:prefix/',
-]);
-
-Router::connect('/:api/:prefix/users/lookup', [
-    'plugin' => 'users',
-    'controller' => 'users',
-    'action' => 'lookup',
-], [
-    'routeClass' => 'Croogo\Core\Routing\Route\ApiRoute',
-]);
-
-// Users
-CroogoRouter::connect('/register', ['plugin' => 'Croogo/Users', 'controller' => 'Users', 'action' => 'add']);
-
-CroogoRouter::connect('/user/:username', [
-    'plugin' => 'Croogo/Users', 'controller' => 'Users', 'action' => 'view'], ['pass' => ['username']
-    ]);
-
-    CroogoRouter::connect('/users/:controller/:action/*', [
-    'plugin' => 'Croogo/Users'
-    ]);
-
-    CroogoRouter::connect('/admin/users/:controller/:action/*', [
-    'prefix' => 'admin',
-    'plugin' => 'Croogo/Users'
-    ]);
