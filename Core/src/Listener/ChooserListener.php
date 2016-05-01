bb3db69f3f1959a6d9d4a4b36b536043e137669d
Marlin Cremers <m.cremers@cvo-technologies.com>
2016-05-02 00:55:53 +0200

    Add listener for chooser pages

---
+++ Core/src/Listener/ChooserListener.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Croogo\Core\Listener;
+
+use Cake\Event\Event;
+use Crud\Listener\BaseListener;
+
+class ChooserListener extends BaseListener
+{
+    public function beforeFilter(Event $event)
+    {
+        if (!$this->_controller()->request->query('chooser')) {
+            return;
+        }
+
+        $this->_controller()->viewBuilder()->layout('Croogo/Core.admin_popup');
+    }
+}
