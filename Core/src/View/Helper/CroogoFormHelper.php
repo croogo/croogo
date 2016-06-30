bf0aaa7c4de5d8b90df608f2fb6771fa096e30ce
Walther Lalk <waltherlalk@gmail.com>
2016-06-30 11:16:03 +0200

    Improve display of help and error messages in admin

--- Core/src/View/Helper/CroogoFormHelper.php
+++ Core/src/View/Helper/CroogoFormHelper.php
@@ -48,7 +48,9 @@ class CroogoFormHelper extends FormHelper
 
         $this->_templates['dateWidget'] = '<ul class="list-inline"><li class="year list-inline-item">{{year}}</li><li class="month list-inline-item">{{month}}</li><li class="day list-inline-item">{{day}}</li><li class="hour list-inline-item">{{hour}}</li><li class="minute list-inline-item">{{minute}}</li><li class="second list-inline-item">{{second}}</li><li class="meridian list-inline-item">{{meridian}}</li></ul>';
         $this->_templates['file'] = '<input type="file" name="{{name}}"{{attrs}}><span class="file-custom"></span>';
-        $this->_templates['inputContainerError'] = '<div class="form-group {{type}}{{required}} has-danger">{{content}}{{error}}{{help}}</div>';
+        $this->_templates['inputContainerError'] = '<div class="form-group {{type}}{{required}} has-danger">{{content}}{{help}}{{error}}</div>';
+        $this->_templates['error'] = '<p class="text-danger">{{content}}</p>';
+        $this->_templates['help'] = '<small class="text-muted">{{content}}</small>';
 
         parent::__construct($View, $settings);
     }
