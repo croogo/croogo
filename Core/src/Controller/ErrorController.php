fbc197fd9372995be91c3b549c05553aec9dddc3
Walther Lalk <waltherlalk@gmail.com>
2016-07-03 21:04:15 +0200

    Error controller should extend core error controller

--- Core/src/Controller/ErrorController.php
+++ Core/src/Controller/ErrorController.php
@@ -21,7 +21,7 @@ use Cake\Routing\Router;
  * @license  http://www.opensource.org/licenses/mit-license.php The MIT License
  * @link     http://www.croogo.org
  */
-class ErrorController extends AppController
+class ErrorController extends \Cake\Controller\ErrorController
 {
     public function initialize()
     {
