78c1d8a91f19a891b4988f43f7bf7b8482b4ac27
Walther Lalk <waltherlalk@gmail.com>
2016-07-29 09:49:29 +0200

    Add default password value

---
+++ Users/config/Migrations/20160729074842_DefaultPasswordValue.php
@@ -0,0 +1,23 @@
+<?php
+use Migrations\AbstractMigration;
+
+class DefaultPasswordValue extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('users');
+        $table->addColumn('password', 'string', [
+                'default' => '',
+                'limit' => 100,
+                'null' => false,
+            ])
+            ->update();
+    }
+}
