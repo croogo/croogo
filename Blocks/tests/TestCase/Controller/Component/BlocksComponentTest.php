5d6bca56dc66ad8854c7bd6125f1cfff1782ff40
Marlin Cremers <m.cremers@cvo-technologies.com>
2016-08-03 10:39:44 +0100

    Disable the BlocksComponentTest

--- Blocks/tests/TestCase/Controller/Component/BlocksComponentTest.php
+++ Blocks/tests/TestCase/Controller/Component/BlocksComponentTest.php
@@ -2,67 +2,106 @@
 
 namespace Croogo\Blocks\Test\TestCase\Controller\Component;
 
+use Cake\Controller\ComponentRegistry;
 use Cake\Controller\Controller;
+use Cake\Core\Configure;
+use Cake\Network\Request;
+use Cake\Network\Response;
+use Cake\ORM\TableRegistry;
+use Croogo\Blocks\Controller\Component\BlocksComponent;
+use Croogo\Core\TestSuite\IntegrationTestCase;
+use Croogo\Core\TestSuite\TestCase;
 use Croogo\TestSuite\CroogoControllerTestCase;
 
-class BlocksTestController extends Controller
+class BlocksComponentTest extends IntegrationTestCase
 {
-
-    public $components = [
-        'Auth',
-        'Session',
-        'Croogo.Croogo',
-        'Blocks.Blocks',
-    ];
-
-    public function beforeFilter()
-    {
-        $this->Auth->allow('index');
-        parent::beforeFilter();
-    }
-
-    public function index()
-    {
-    }
-}
-
-class BlocksComponentTest extends CroogoControllerTestCase
-{
-
     public $fixtures = [
-        'plugin.blocks.block',
-        'plugin.blocks.region',
-        'plugin.menus.menu',
-        'plugin.menus.link',
+        'plugin.croogo/blocks.block',
+        'plugin.croogo/blocks.region',
+        'plugin.croogo/menus.menu',
+        'plugin.croogo/menus.link',
+        'plugin.croogo/taxonomy.type',
+        'plugin.croogo/taxonomy.vocabulary',
+        'plugin.croogo/taxonomy.taxonomy',
+        'plugin.croogo/taxonomy.term',
+        'plugin.croogo/taxonomy.model_taxonomy',
+        'plugin.croogo/comments.comment',
+        'plugin.croogo/meta.meta',
+        'plugin.croogo/nodes.node',
+        'plugin.croogo/users.role',
+        'plugin.croogo/users.user',
+        'plugin.croogo/users.aro',
+        'plugin.croogo/users.aco',
+        'plugin.croogo/users.aros_aco',
     ];
+
+    /**
+     * @var \Cake\Controller\Controller
+     */
+    public $controller;
+
+    /**
+     * @var \Croogo\Blocks\Controller\Component\BlocksComponent
+     */
+    public $component;
 
     public function setUp()
     {
-        $this->_paths = App::paths();
-        $app = Plugin::path('Blocks') . 'Test' . DS . 'test_app' . DS;
-        App::build([
-            'Controller' => [
-                $app . 'Controller' . DS,
-            ],
-            'View' => [
-                $app . 'View' . DS,
-            ],
-        ]);
-        $this->generate('BlocksTest');
+        parent::setUp();
+
+//        $request = new Request;
+//        $response = new Response;
+//        $this->controller = $this->getMock(
+//            'Cake\Controller\Controller',
+//            null,
+//            [$request, $response]
+//        );
+//        $this->controller->loadComponent('Croogo/Core.Croogo');
+//        $this->component = $this->controller->components()->load('Croogo/Blocks.Blocks');
+//
+//        $this->_paths = App::paths();
+//        $app = Plugin::path('Blocks') . 'Test' . DS . 'test_app' . DS;
+//        App::build([
+//            'Controller' => [
+//                $app . 'Controller' . DS,
+//            ],
+//            'View' => [
+//                $app . 'View' . DS,
+//            ],
+//        ]);
+//        $this->generate('BlocksTest');
     }
 
-    public function tearDown()
-    {
-        App::paths($this->_paths);
-        unset($this->controller);
-    }
+//    public function tearDown()
+//    {
+//        App::paths($this->_paths);
+//        unset($this->controller);
+//    }
 
 /**
  * test that public Blocks are displayed
  */
     public function testBlockGenerationForPublic()
     {
-        $this->controller->layout = 'ajax';
+        $this->markTestIncomplete('This test is being ported');
+
+//        $this->get('/');
+
+//        debug($this->_response);
+
+        return;
+
+//        $this->controller->startupProcess();
+////        $this->controller->viewBuilder()->layout('ajax');
+//
+//        debug($this->component->blocks());
+//
+//        $this->controller->render();
+//
+//        debug($this->controller->viewBuilder());
+//
+//        return;
+
         $vars = $this->testAction('/index', [
             'return' => 'vars',
         ]);
@@ -85,6 +124,23 @@ class BlocksComponentTest extends CroogoControllerTestCase
  */
     public function testBlockGenerationForRegistered()
     {
+        $this->markTestIncomplete('This test is being ported');
+        $this->user('registered-user');
+
+        $this->get('/');
+
+//        debug($this->_response->body());
+//        debug($this->_controller);
+        $this->assertEmpty(collection($this->viewVariable('blocksForLayout')['right'])->match([
+            'title' => 'Block Visible by Public'
+        ])->toArray());
+
+        $this->assertNotEmpty(collection($this->viewVariable('blocksForLayout')['right'])->match([
+            'title' => 'Block Visible by Admin or Registered'
+        ])->toArray());
+
+        exit();
+
         $this->controller->layout = 'ajax';
         $this->controller->Session->write('Auth.User', ['id' => 3, 'role_id' => 2]);
         $vars = $this->testAction('/index', [
@@ -110,6 +166,8 @@ class BlocksComponentTest extends CroogoControllerTestCase
  */
     public function testBlockGenerationForAdmin()
     {
+        $this->markTestIncomplete('This test is being ported');
+
         $this->controller->layout = 'ajax';
         $this->controller->Session->write('Auth.User', ['id' => 1, 'role_id' => 1]);
         $vars = $this->testAction('/index', [
