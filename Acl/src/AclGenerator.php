48ebdd907a2335d24c9839a99133d75daae5cc31
Marlin Cremers <m.cremers@cvo-technologies.com>
2016-08-03 10:40:15 +0100

    Add tool to generate Acls programatically

---
+++ Acl/src/AclGenerator.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Croogo\Acl;
+
+use Acl\AclExtras;
+use Cake\Database\Exception;
+use Cake\Datasource\ConnectionInterface;
+use Cake\ORM\TableRegistry;
+use Croogo\Core\Plugin;
+
+class AclGenerator extends AclExtras
+{
+    public function __construct()
+    {
+        $this->Aco = TableRegistry::get('Croogo/Acl.Acos');
+        $this->_buildPrefixes();
+    }
+
+    public function insertAcos(ConnectionInterface $connection)
+    {
+        $this->Aco->connection($connection);
+
+        $root = $this->_checkNode($this->rootNode, $this->rootNode, null);
+        $plugins = Plugin::loaded();
+        $this->_processControllers($root);
+        $this->_processPrefixes($root);
+        $this->_processPlugins($root, $plugins);
+    }
+
+    public function out($msg)
+    {
+        preg_match('/Created Aco node: \<success\>(.*)\<\/success\>/', $msg, $matches);
+//        echo($matches[1] . PHP_EOL);
+    }
+
+    protected function _checkMethods($className, $controllerName, $node, $pluginPath = null, $prefixPath = null)
+    {
+        try {
+            return parent::_checkMethods($className, $controllerName, $node, $pluginPath, $prefixPath);
+        } catch (Exception $exception) {
+        }
+
+        return false;
+    }
+}
