d7cd78898bbaad37c0b02716c9199b80ddeed830
Marlin Cremers <m.cremers@cvo-technologies.com>
2016-08-07 00:30:03 +0100

    Include Croogo plugins and themes in cakephp-plugins.php

--- Install/src/ComposerInstaller.php
+++ Install/src/ComposerInstaller.php
@@ -30,4 +30,45 @@ class ComposerInstaller extends PluginInstaller
         $configFile = static::_configFile($vendorDir);
         static::writeConfigFile($configFile, $plugins);
     }
+    /**
+     * Find all plugins available
+     *
+     * Add all composer packages of type cakephp-plugin, and all plugins located
+     * in the plugins directory to a plugin-name indexed array of paths
+     *
+     * @param array $packages an array of \Composer\Package\PackageInterface objects
+     * @param string $pluginsDir the path to the plugins dir
+     * @param string $vendorDir the path to the vendor dir
+     * @return array plugin-name indexed paths to plugins
+     */
+    public static function determinePlugins($packages, $pluginsDir = 'plugins', $vendorDir = 'vendor')
+    {
+        $plugins = [];
+
+        foreach ($packages as $package) {
+            if (!in_array($package->getType(), ['cakephp-plugin', 'croogo-plugin', 'croogo-theme'])) {
+                continue;
+            }
+
+            $ns = static::primaryNamespace($package);
+            $path = $vendorDir . DIRECTORY_SEPARATOR . $package->getPrettyName();
+            $plugins[$ns] = $path;
+        }
+
+        if (is_dir($pluginsDir)) {
+            $dir = new \DirectoryIterator($pluginsDir);
+            foreach ($dir as $info) {
+                if (!$info->isDir() || $info->isDot()) {
+                    continue;
+                }
+
+                $name = $info->getFilename();
+                $plugins[$name] = $pluginsDir . DIRECTORY_SEPARATOR . $name;
+            }
+        }
+
+        ksort($plugins);
+        return $plugins;
+    }
+
 }
