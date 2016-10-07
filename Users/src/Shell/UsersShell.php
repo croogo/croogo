55d305e255cdbf5475911cb1a1c69451c053b635
Rachman Chavik <rchavik@xintesa.com>
2016-10-07 20:06:06 +0700

    Add new shell method to create new user

--- Users/src/Shell/UsersShell.php
+++ Users/src/Shell/UsersShell.php
@@ -3,6 +3,7 @@
 namespace Croogo\Users\Shell;
 
 use Cake\Console\Shell;
+use Croogo\Users\Model\Entity\User;
 
 /**
  * UsersShell
@@ -31,6 +32,25 @@ class UsersShell extends Shell
     public function getOptionParser()
     {
         return parent::getOptionParser()
+            ->addSubCommand('create', [
+                'help' => __d('croogo', 'Create a new user'),
+                'parser' => [
+                    'arguments' => [
+                        'username' => [
+                            'required' => true,
+                            'help' => __d('croogo', 'Username to reset'),
+                        ],
+                        'password' => [
+                            'required' => true,
+                            'help' => __d('croogo', 'New user password'),
+                        ],
+                        'role_id' => [
+                            'required' => true,
+                            'help' => __d('croogo', 'Role id for user'),
+                        ],
+                    ],
+                ],
+            ])
             ->addSubCommand('reset', [
                 'help' => __d('croogo', 'Reset user password'),
                 'parser' => [
@@ -65,6 +85,35 @@ class UsersShell extends Shell
         $result = $this->Users->save($user);
         if ($result) {
             $this->success(__d('croogo', 'Password for \'%s\' has been changed', $username));
+        }
+    }
+
+    /**
+     * reset
+     */
+    public function create()
+    {
+        $username = $this->args[0];
+        $password = $this->args[1];
+        $roleId = $this->args[2];
+
+        $user = $this->Users->findByUsername($username)->first();
+        if ($user) {
+            return $this->warn(__d('croogo', 'User \'%s\' already exists', $username));
+        }
+
+        $user = new User([
+            'username' => $username,
+            'password' => $password,
+            'role_id' => $roleId,
+            'name' => $username,
+            'email' => $username,
+            'activation_key' => bin2hex(openssl_random_pseudo_bytes(20)),
+            'status' => true,
+        ]);
+        $result = $this->Users->save($user);
+        if ($result) {
+            $this->success(__d('croogo', 'User \'%s\' has been created', $username));
         }
     }
 
