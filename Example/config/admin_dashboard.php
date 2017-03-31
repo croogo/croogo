381640d59ce04cfec10cb324edab1d637f990403
Rachman Chavik <rchavik@xintesa.com>
2017-03-31 21:41:47 +0700

    Update example dashboard

--- Example/config/admin_dashboard.php
+++ Example/config/admin_dashboard.php
@@ -3,7 +3,7 @@
 $config = [
     'example.welcome' => [
         'title' => __d('croogo', 'Welcome'),
-        'element' => 'Croogo/Example.dashboard/welcome',
+        'cell' => 'Croogo/Example.ExampleDashboard::welcome',
         'weight' => 1,
         'column' => \Croogo\Dashboards\CroogoDashboard::FULL,
     ],
---
+++ Example/src/View/Cell/ExampleDashboardCell.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Croogo\Example\View\Cell;
+
+use Cake\Cache\Cache;
+use Cake\I18n\Time;
+use Cake\Utility\Xml;
+use Cake\View\Cell;
+use Croogo\Core\Link;
+
+class ExampleDashboardCell extends Cell
+{
+
+    public function welcome()
+    {
+    }
+
+}
