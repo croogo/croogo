46ee31438288e92ae876c7139420b89707fc9563
Rachman Chavik <rchavik@xintesa.com>
2017-05-26 12:46:08 +0700

    Translate: [Fix] Avoid stripping of translated fields
    When creating a new record, we need to set this property.  Without it,
    the Behavior may strip out fields as it thinks that data being saved is a
    empty translation record.
    
    To avoid this, we populate `_locale` from default locale when it's empty.

--- Translate/src/Translations.php
+++ Translate/src/Translations.php
@@ -11,9 +11,9 @@
 namespace Croogo\Translate;
 
 use Cake\Core\Configure;
+use Cake\Log\Log;
 use Cake\Utility\Inflector;
 use Croogo\Core\Croogo;
-use \Cake\Log\Log;
 
 class Translations
 {
@@ -23,12 +23,12 @@ class Translations
  */
     public static function translateModels()
     {
-        $path ='admin:true/plugin:Croogo%2fTranslate/controller:Translate/action:index/?id=:id&model={{model}}';
+        $path ='prefix:admin/plugin:Croogo%2fTranslate/controller:Translate/action:index/?id=:id&model={{model}}';
         foreach (Configure::read('Translate.models') as $encoded => $config) {
             $model = base64_decode($encoded);
-            Croogo::hookBehavior($model, 'Translate', $config);
+            Croogo::hookBehavior($model, 'Croogo/Translate.Translate', $config);
             $action = str_replace('.', '.Admin/', $model . '/index');
-            $url = str_replace('{{model}}', $model, $path);
+            $url = str_replace('{{model}}', urlencode($model), $path);
             Croogo::hookAdminRowAction($action,
                 __d('croogo', 'Translate'),
                 [
