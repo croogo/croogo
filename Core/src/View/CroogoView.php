6124757050cf4558ecee67ff0154b32a1431367a
Rachman Chavik <rchavik@xintesa.com>
2017-09-19 12:11:44 +0700

    Add ability to override theme templates from APP
    See cakephp/cakephp#11198

--- Core/src/View/CroogoView.php
+++ Core/src/View/CroogoView.php
@@ -3,6 +3,7 @@
 namespace Croogo\Core\View;
 
 use App\View\AppView;
+use Cake\Core\App;
 use Cake\Utility\Hash;
 use Cake\Utility\Inflector;
 use Croogo\Core\Croogo;
@@ -16,6 +17,57 @@ use Croogo\Extensions\CroogoTheme;
  */
 class CroogoView extends AppView
 {
+
+    /**
+     * Return all possible paths to find view files in order
+     *
+     * @param string|null $plugin Optional plugin name to scan for view files.
+     * @param bool $cached Set to false to force a refresh of view paths. Default true.
+     * @return array paths
+     */
+    protected function _paths($plugin = null, $cached = true)
+    {
+        if ($cached === true) {
+            if ($plugin === null && !empty($this->_paths)) {
+                return $this->_paths;
+            }
+            if ($plugin !== null && isset($this->_pathsForPlugin[$plugin])) {
+                return $this->_pathsForPlugin[$plugin];
+            }
+        }
+        $templatePaths = App::path(static::NAME_TEMPLATE);
+        $pluginPaths = $themePaths = [];
+        if (!empty($plugin)) {
+            for ($i = 0, $count = count($templatePaths); $i < $count; $i++) {
+                $pluginPaths[] = $templatePaths[$i] . 'Plugin' . DIRECTORY_SEPARATOR . $plugin . DIRECTORY_SEPARATOR;
+            }
+            $pluginPaths = array_merge($pluginPaths, App::path(static::NAME_TEMPLATE, $plugin));
+        }
+
+        if (!empty($this->theme)) {
+            $themePaths = App::path(static::NAME_TEMPLATE, Inflector::camelize($this->theme));
+            array_unshift($themePaths, APP . 'Template' . DIRECTORY_SEPARATOR . 'Plugin' . DIRECTORY_SEPARATOR . $this->theme . DIRECTORY_SEPARATOR);
+
+            if ($plugin) {
+                foreach (array_reverse($themePaths) as $path) {
+                    array_unshift($themePaths, $path . 'Plugin' . DIRECTORY_SEPARATOR . $plugin . DIRECTORY_SEPARATOR);
+                }
+            }
+        }
+
+        $paths = array_merge(
+            $themePaths,
+            $pluginPaths,
+            $templatePaths,
+            [dirname(__DIR__) . DIRECTORY_SEPARATOR . static::NAME_TEMPLATE . DIRECTORY_SEPARATOR]
+        );
+
+        if ($plugin !== null) {
+            return $this->_pathsForPlugin[$plugin] = $paths;
+        }
+
+        return $this->_paths = $paths;
+    }
 
     public function loadHelpers()
     {
