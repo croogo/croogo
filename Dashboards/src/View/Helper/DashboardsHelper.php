b04cb407b9c86db0267db9e8fa4381bd0aea4c24
Rachman Chavik <rchavik@xintesa.com>
2017-09-26 11:38:09 +0700

    Dashboards: remove &nbsp;

--- Dashboards/src/View/Helper/DashboardsHelper.php
+++ Dashboards/src/View/Helper/DashboardsHelper.php
@@ -127,16 +127,16 @@ class DashboardsHelper extends Helper
 
         $dashboardTag = $this->settings['dashboardTag'];
         $columnDivs = [
-            0 => $this->Html->tag($dashboardTag, implode('', $columns[CroogoDashboard::LEFT]) . '&nbsp;', [
+            0 => $this->Html->tag($dashboardTag, implode('', $columns[CroogoDashboard::LEFT]) . ' ', [
                 'class' => $cssSetting['dashboardLeft'] . ' ' . $cssSetting['dashboardClass'],
                 'id' => 'column-0',
             ]),
-            1 => $this->Html->tag($dashboardTag, implode('', $columns[CroogoDashboard::RIGHT]) . '&nbsp;', [
+            1 => $this->Html->tag($dashboardTag, implode('', $columns[CroogoDashboard::RIGHT]) . ' ', [
                 'class' => $cssSetting['dashboardRight'] . ' ' . $cssSetting['dashboardClass'],
                 'id' => 'column-1'
             ]),
         ];
-        $fullDiv = $this->Html->tag($dashboardTag, implode('', $columns[CroogoDashboard::FULL]) . '&nbsp;', [
+        $fullDiv = $this->Html->tag($dashboardTag, implode('', $columns[CroogoDashboard::FULL]) . ' ', [
             'class' => $cssSetting['dashboardFull'] . ' ' . $cssSetting['dashboardClass'],
             'id' => 'column-2',
         ]);
