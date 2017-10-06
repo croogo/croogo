3d305a31884a14e8d2c799cae4b2aba6fe846628
Rachman Chavik <rchavik@xintesa.com>
2017-10-06 09:53:04 +0700

    Differentiate error message

--- Users/src/Controller/UsersController.php
+++ Users/src/Controller/UsersController.php
@@ -233,6 +233,7 @@ class UsersController extends AppController
 
         if (!$this->Access->isUrlAuthorized($user, $redirectUrl)) {
             Croogo::dispatchEvent('Controller.Users.loginFailure', $this);
+            $this->Auth->config('authError', __d('croogo', 'Authorization error'));
             $this->Flash->error($this->Auth->config('authError'));
             return $this->redirect($this->Auth->loginRedirect);
         }
