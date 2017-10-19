a507c8120b35cf300ff2aa7a5958cd1d4dcf1578
Rachman Chavik <rchavik@xintesa.com>
2017-10-19 09:54:49 +0700

    Fix legacy bs4 class name

--- Meta/webroot/js/admin.js
+++ Meta/webroot/js/admin.js
@@ -30,7 +30,7 @@ Meta.addMeta = function () {
       .addClass('disabled')
       .prepend(Meta._spinner);
     $.get(aAddMeta.attr('href'), function (data) {
-      aAddMeta.closest('.card-block').find('.meta-fields').append(data);
+      aAddMeta.closest('.card-body').find('.meta-fields').append(data);
       $('div.meta a.remove-meta').unbind();
       Meta.removeMeta();
       aAddMeta
