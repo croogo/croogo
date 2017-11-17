26465d2d82f50ae76de06a22a5eb7ed40c2b2f2b
Rachman Chavik <rchavik@xintesa.com>
2017-11-17 11:51:14 +0700

    Fix: Don't use Site.status to detect successful install

--- Acl/config/bootstrap.php
+++ Acl/config/bootstrap.php
@@ -7,7 +7,7 @@ use Croogo\Core\Croogo;
 
 if (Configure::read('Site.acl_plugin') == 'Croogo/Acl') {
     // activate AclFilter component only until after a succesfull install
-    if (Configure::read('Site.status')) {
+    if (Configure::read('Croogo.installed')) {
         Croogo::hookComponent('*', 'Croogo/Acl.Filter');
         Croogo::hookComponent('*', 'Croogo/Acl.Access');
     }
