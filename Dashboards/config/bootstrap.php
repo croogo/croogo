b840a0f188fc9d6cb830dd0d45b674bf8f58b568
Rachman Chavik <rchavik@xintesa.com>
2017-12-27 18:45:52 +0700

    Don't overwrite Croogo.dashboardUrl value from config

--- Dashboards/config/bootstrap.php
+++ Dashboards/config/bootstrap.php
@@ -7,11 +7,13 @@
 use Cake\Core\Configure;
 use Croogo\Dashboards\Configure\DashboardsConfigReader;
 
-Configure::write('Croogo.dashboardUrl', [
-    'prefix' => 'admin',
-    'plugin' => 'Croogo/Dashboards',
-    'controller' => 'Dashboards',
-    'action' => 'dashboard',
-]);
+if (!Configure::check('Croogo.dashboardUrl')) {
+    Configure::write('Croogo.dashboardUrl', [
+        'prefix' => 'admin',
+        'plugin' => 'Croogo/Dashboards',
+        'controller' => 'Dashboards',
+        'action' => 'dashboard',
+    ]);
+}
 
 Configure::config('dashboards', new DashboardsConfigReader());
