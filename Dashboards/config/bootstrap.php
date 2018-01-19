d103d11e942d5410b49ee9b7a74a62a478032349
Rachman Chavik <rchavik@xintesa.com>
2018-01-19 19:31:25 +0700

    Fix: leftover dashboardUrl and convert to expected value

--- Dashboards/config/bootstrap.php
+++ Dashboards/config/bootstrap.php
@@ -6,14 +6,16 @@
 
 use Cake\Core\Configure;
 use Croogo\Dashboards\Configure\DashboardsConfigReader;
+use Croogo\Core\Utility\StringConverter;
 
-if (!Configure::check('Site.dashboardUrl')) {
-    Configure::write('Site.dashboardUrl', [
+if (!Configure::check('Site.dashboard_url')) {
+    $converter = new StringConverter();
+    Configure::write('Site.dashboard_url', $converter->urlToLinkString([
         'prefix' => 'admin',
         'plugin' => 'Croogo/Dashboards',
         'controller' => 'Dashboards',
         'action' => 'dashboard',
-    ]);
+    ]));
 }
 
 Configure::config('dashboards', new DashboardsConfigReader());
