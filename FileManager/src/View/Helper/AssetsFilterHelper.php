5ccc85359ce2fdb368d4a26f26a4822870ca3921
Rachman Chavik <rchavik@xintesa.com>
2019-08-29 21:21:30 +0700

    Remove copied code from StringConverter

--- FileManager/src/View/Helper/AssetsFilterHelper.php
+++ FileManager/src/View/Helper/AssetsFilterHelper.php
@@ -7,6 +7,7 @@ use Cake\Log\LogTrait;
 use Cake\ORM\TableRegistry;
 use Cake\View\View;
 use Cake\View\Helper;
+use Croogo\Core\Utility\StringConverter;
 
 class AssetsFilterHelper extends Helper {
 
@@ -34,10 +35,10 @@ class AssetsFilterHelper extends Helper {
         }
     }
 
-    //public function filter(&$content, $options = array()) {
     public function filter(Event $event) {
         $content =& $event->result['content'];
         $options =& $event->result['options'];
+        $converter = new StringConverter();
         $conditions = array();
         $identifier = '';
         if (isset($options['model']) && isset($options['id'])) {
@@ -52,7 +53,7 @@ class AssetsFilterHelper extends Helper {
         $AssetUsages = TableRegistry::get('Croogo/FileManager.AssetUsages');
 
         for ($i = 0, $ii = count($tagMatches[1]); $i < $ii; $i++) {
-            $assets = $this->parseString('image|i', $tagMatches[0][$i]);
+            $assets = $converter->parseString('image|i', $tagMatches[0][$i]);
             $assetId = $tagMatches[2][$i];
             $conditions['AssetUsages.id'] = $assetId;
             $assetUsage = $AssetUsages->find()
@@ -90,77 +91,6 @@ class AssetsFilterHelper extends Helper {
         ]));
 
         $this->Nodes->field('body', $body);
-    }
-
-/**
- * Parses bb-code like string.
- *
- * Example: string containing [menu:main option1="value"] will return an array like
- *
- * Array
- * (
- *     [main] => Array
- *         (
- *             [option1] => value
- *         )
- * )
- *
- * @param string $exp
- * @param string $text
- * @param array  $options
- * @return array
- */
-    public function parseString($exp, $text, $options = array()) {
-        $_options = array(
-            'convertOptionsToArray' => false,
-        );
-        $options = array_merge($_options, $options);
-
-        $output = array();
-        preg_match_all('/\[(' . $exp . '):([A-Za-z0-9_\-]*)(.*?)\]/i', $text, $tagMatches);
-        for ($i = 0, $ii = count($tagMatches[1]); $i < $ii; $i++) {
-            $regex = '/(\S+)=[\'"]?((?:.(?![\'"]?\s+(?:\S+)=|[>\'"]))+.)[\'"]?/i';
-            preg_match_all($regex, $tagMatches[3][$i], $attributes);
-            $alias = $tagMatches[2][$i];
-            $aliasOptions = array();
-            for ($j = 0, $jj = count($attributes[0]); $j < $jj; $j++) {
-                $aliasOptions[$attributes[1][$j]] = $attributes[2][$j];
-            }
-            if ($options['convertOptionsToArray']) {
-                foreach ($aliasOptions as $optionKey => $optionValue) {
-                    if (!is_array($optionValue) && strpos($optionValue, ':') !== false) {
-                        $aliasOptions[$optionKey] = $this->stringToArray($optionValue);
-                    }
-                }
-            }
-            $output[$alias] = $aliasOptions;
-        }
-        return $output;
-    }
-
-/**
- * Converts formatted string to array
- *
- * A string formatted like 'Node.type:blog;' will be converted to
- * array('Node.type' => 'blog');
- *
- * @param string $string in this format: Node.type:blog;Node.user_id:1;
- * @return array
- */
-    public function stringToArray($string) {
-        $string = explode(';', $string);
-        $stringArr = array();
-        foreach ($string as $stringElement) {
-            if ($stringElement != null) {
-                $stringElementE = explode(':', $stringElement);
-                if (isset($stringElementE['1'])) {
-                    $stringArr[$stringElementE['0']] = $stringElementE['1'];
-                } else {
-                    $stringArr[] = $stringElement;
-                }
-            }
-        }
-        return $stringArr;
     }
 
 }
