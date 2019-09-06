af3bee0ca798a0a7b8c907036b01482714039d58
Rachman Chavik <rchavik@xintesa.com>
2019-09-06 10:44:15 +0700

    Fix: Use methods instead of array to set relationships

--- Core/src/PropertyHookTrait.php
+++ Core/src/PropertyHookTrait.php
@@ -2,11 +2,19 @@
 
 namespace Croogo\Core;
 
+use Cake\Utility\Hash;
+
 trait PropertyHookTrait
 {
 
     public function getProperty($property)
     {
+        $relTypes = ['hasOne', 'hasMany', 'belongsTo', 'belongsToMany'];
+        if (in_array($property, $relTypes)) {
+            $associations = $this->associations();
+            return $associations ? $associations->has($property) : false;
+        }
+
         if (!isset($this->{$property})) {
             return false;
         }
@@ -15,6 +23,12 @@ trait PropertyHookTrait
 
     public function setProperty($property, $value, $merge = false)
     {
+        $relTypes = ['hasOne', 'hasMany', 'belongsTo', 'belongsToMany'];
+        if (in_array($property, $relTypes)) {
+            $associations = $this->associations();
+            return $associations ? $associations->addAssociations($property, $value) : false;
+        }
+
         if ($merge && $this->{$property}) {
             $value = Hash::merge($this->{$property}, $value);
         }
