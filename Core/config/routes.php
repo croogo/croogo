f2aedcbf73c3b0fe08f844790f6336330e8858f7
Rachman Chavik <rchavik@xintesa.com>
2019-11-09 12:47:20 +0700

    Core: Add Csrf middleware for /admin with httpOnly = false
    See 3e1aac4435f17b2f80226ed8c9c9cbb56ddce1ba

--- Core/config/routes.php
+++ Core/config/routes.php
@@ -1,11 +1,14 @@
 <?php
 
 use Cake\Core\Configure;
+use Cake\Http\Middleware\CsrfProtectionMiddleware;
 use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
 use Croogo\Core\Utility\StringConverter;
 
 Router::prefix('admin', function (RouteBuilder $routeBuilder) {
+    $routeBuilder->registerMiddleware('csrf', new CsrfProtectionMiddleware());
+    $routeBuilder->applyMiddleware('csrf');
 
     $dashboardUrl = Configure::read('Site.dashboard_url');
     if (!$dashboardUrl) {
