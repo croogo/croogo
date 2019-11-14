7947f82fae5253f6160ba77b04fa0ecd6a1f0a19
Rachman Chavik <rchavik@xintesa.com>
2019-11-14 09:08:31 +0700

    FileManager: Use helper so that querystring is correctly generated

--- FileManager/src/View/Helper/FileManagerHelper.php
+++ FileManager/src/View/Helper/FileManagerHelper.php
@@ -2,7 +2,6 @@
 
 namespace Croogo\FileManager\View\Helper;
 
-use Cake\Routing\Router;
 use Cake\View\Helper;
 
 /**
@@ -165,7 +164,10 @@ class FileManagerHelper extends Helper
         if (isset($url['action']) && in_array($url['action'], $this->__postLinkActions)) {
             $output = $this->Form->postLink($title, $url, ['data' => compact('path'), 'escape' => true], __d('croogo', 'Are you sure?'));
         } else {
-            $output = '<a class="' . $class . '" href="' . Router::url($url) . "?{$pathKey}=" . urlencode($path) . '">' . $title . '</a>';
+            $url[$pathKey] = $path;
+            $output = $this->Html->link($title, $url, [
+                'class' => $class,
+            ]);
         }
 
         return $output;
