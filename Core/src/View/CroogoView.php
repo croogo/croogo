dc5dd8b1a474d9c30ee767fd990400cb336fc4ba
Rachman Chavik <39490+rchavik@users.noreply.github.com>
2020-09-19 20:26:33 +0700

    Fix: Expect missing templates due to extension
    Eg: /page/about.rss will cause error500 which is not correct.
    Also remove outdated hack with _paths().

--- Core/src/View/CroogoView.php
+++ Core/src/View/CroogoView.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 namespace Croogo\Core\View;
 
 use App\View\AppView;
-use Cake\Core\App;
-use Cake\Utility\Inflector;
+use Cake\Http\Exception\NotFoundException;
+use Cake\View\Exception\MissingTemplateException;
 use Croogo\Core\Croogo;
 use Croogo\Extensions\CroogoTheme;
 
@@ -17,57 +17,6 @@ use Croogo\Extensions\CroogoTheme;
  */
 class CroogoView extends AppView
 {
-
-    /**
-     * Return all possible paths to find view files in order
-     *
-     * @param string|null $plugin Optional plugin name to scan for view files.
-     * @param bool $cached Set to false to force a refresh of view paths. Default true.
-     * @return array paths
-     */
-    protected function _paths(?string $plugin = null, bool $cached = true): array
-    {
-        if ($cached === true) {
-            if ($plugin === null && !empty($this->_paths)) {
-                return $this->_paths;
-            }
-            if ($plugin !== null && isset($this->_pathsForPlugin[$plugin])) {
-                return $this->_pathsForPlugin[$plugin];
-            }
-        }
-        $templatePaths = App::path(static::NAME_TEMPLATE);
-        $pluginPaths = $themePaths = [];
-        if (!empty($plugin)) {
-            for ($i = 0, $count = count($templatePaths); $i < $count; $i++) {
-                $pluginPaths[] = $templatePaths[$i] . 'Plugin' . DIRECTORY_SEPARATOR . $plugin . DIRECTORY_SEPARATOR;
-            }
-            $pluginPaths = array_merge($pluginPaths, App::path(static::NAME_TEMPLATE, $plugin));
-        }
-
-        if (!empty($this->theme)) {
-            $themePaths = App::path(static::NAME_TEMPLATE, Inflector::camelize($this->theme));
-            array_unshift($themePaths, APP . 'Template' . DIRECTORY_SEPARATOR . 'Plugin' . DIRECTORY_SEPARATOR . $this->theme . DIRECTORY_SEPARATOR);
-
-            if ($plugin) {
-                foreach (array_reverse($themePaths) as $path) {
-                    array_unshift($themePaths, $path . 'Plugin' . DIRECTORY_SEPARATOR . $plugin . DIRECTORY_SEPARATOR);
-                }
-            }
-        }
-
-        $paths = array_merge(
-            $themePaths,
-            $pluginPaths,
-            $templatePaths,
-            [dirname(__DIR__) . DIRECTORY_SEPARATOR . static::NAME_TEMPLATE . DIRECTORY_SEPARATOR]
-        );
-
-        if ($plugin !== null) {
-            return $this->_pathsForPlugin[$plugin] = $paths;
-        }
-
-        return $this->_paths = $paths;
-    }
 
     public function loadHelpers()
     {
@@ -104,4 +53,16 @@ class CroogoView extends AppView
             $this->loadHelper($helper, $config);
         }
     }
+
+    public function render(?string $template = null, $layout = null): string
+    {
+        try {
+            return parent::render($template, $layout);
+        } catch (MissingTemplateException $e) {
+            if ($this->getRequest()->getParam('_ext')) {
+                throw new NotFoundException(null, null);
+            }
+        }
+    }
+
 }
