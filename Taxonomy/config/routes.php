ae40b145a485e59e3e2c2c456ea51ee995c4c555
Rachman Chavik <39490+rchavik@users.noreply.github.com>
2020-10-16 22:02:08 +0700

    Add more taxonomy routes

--- Taxonomy/config/routes.php
+++ Taxonomy/config/routes.php
@@ -15,10 +15,12 @@ $routes->plugin('Croogo/Taxonomy', ['path' => '/'], function (RouteBuilder $rout
         $route->prefix('V10', ['path' => '/v1.0'], function (RouteBuilder $route) {
             $route->setExtensions(['json']);
 
+            $route->connect('/taxonomies', ['controller' => 'Taxonomies', 'action' => 'index']);
             $route->resources('Taxonomies', [
                 'only' => ['index', 'view'],
             ]);
 
+            $route->connect('/terms', ['controller' => 'Terms', 'action' => 'index']);
             $route->resources('Terms', [
                 'only' => ['index', 'view'],
             ]);
@@ -28,6 +30,7 @@ $routes->plugin('Croogo/Taxonomy', ['path' => '/'], function (RouteBuilder $rout
                 'only' => ['index', 'view'],
             ]);
 
+            $route->connect('/vocabularies', ['controller' => 'Vocabularies', 'action' => 'index']);
             $route->resources('Vocabularies', [
                 'only' => ['index', 'view'],
             ]);
