b818b177e5f7f76bb59a1e0fe1e1c97fa90102e1
Rachman Chavik <39490+rchavik@users.noreply.github.com>
2020-10-31 13:05:33 +0700

    Add json-api schema for Blocks and Menus

---
+++ Blocks/src/Schema/JsonApi/BlockSchema.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace Croogo\Blocks\Schema\JsonApi;
+
+use Croogo\Core\Schema\JsonApi\DynamicEntitySchema;
+
+class BlockSchema extends DynamicEntitySchema
+{
+}
---
+++ Menus/src/Schema/JsonApi/LinkSchema.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace Croogo\Menus\Schema\JsonApi;
+
+use Croogo\Core\Schema\JsonApi\DynamicEntitySchema;
+
+class LinkSchema extends DynamicEntitySchema
+{
+}
